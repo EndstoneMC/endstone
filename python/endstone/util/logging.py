fb2e013de7df0bb58cc0a4ede4f869c66934beed
Vincent <magicdroidx@gmail.com>
2023-08-17 14:37:16 +0100

    Enable Delay load on windows for Intellisense in PyCharm

--- python/endstone/util/logging.py
+++ python/endstone/util/logging.py
@@ -1,20 +1,16 @@
 import collections.abc
 
-import endstone._logger
 from endstone._logger import LogLevel
+from endstone._logger import Logger as BaseLogger
 
 __all__ = ["Logger", "LogLevel"]
 
 
-def get_logger(name: str):
-    return Logger(name)
+class Logger(BaseLogger):
+    def __init__(self, name: str) -> None:
+        BaseLogger.__init__(self, name)
 
-
-class Logger:
-    def __init__(self, name: str):
-        self._logger = endstone._logger.Logger.get_logger(name)
-
-    def log(self, level: LogLevel, msg: str, *args) -> None:
+    def log(self, level, msg, *args):
         if args and len(args) == 1 and isinstance(args[0], collections.abc.Mapping) and args[0]:
             args = args[0]
 
@@ -22,22 +18,4 @@ class Logger:
         if args:
             msg = msg % args
 
-        self._logger.log(level, msg)
-
-    def verbose(self, msg: str, *args) -> None:
-        self.log(LogLevel.VERBOSE, msg, *args)
-
-    def info(self, msg: str, *args) -> None:
-        self.log(LogLevel.INFO, msg, *args)
-
-    def warning(self, msg: str, *args) -> None:
-        self.log(LogLevel.WARNING, msg, *args)
-
-    def error(self, msg: str, *args) -> None:
-        self.log(LogLevel.ERROR, msg, *args)
-
-    def set_level(self, level: LogLevel) -> None:
-        self._logger.set_level(level)
-
-    def get_name(self) -> str:
-        return self._logger.get_name()
+        BaseLogger.log(self, level, msg)
