e701687bc82d1e6bc4174d7783c2bf4e2a534fe0
Vincent <magicdroidx@gmail.com>
2023-08-30 12:48:13 +0100

    Override the __str__ method for ChatColor in Python

--- python/endstone/__init__.py
+++ python/endstone/__init__.py
@@ -4,3 +4,5 @@ from packaging.version import Version
 
 __version__ = importlib.metadata.version(__package__)
 __minecraft__version__ = (lambda v: f"{v[0]}.{v[1]}.{v[2]}.{v[3]:02}")([int(a) for a in Version(__version__).release])
+
+from endstone._chat_color import ChatColor
