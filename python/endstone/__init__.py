65d28644072bb526c7f064fe8ea0ae546aba9e32
Vincent <magicdroidx@gmail.com>
2023-08-30 09:52:39 +0100

    Code cleanup

--- python/endstone/__init__.py
+++ python/endstone/__init__.py
@@ -3,12 +3,4 @@ import importlib.metadata
 from packaging.version import Version
 
 __version__ = importlib.metadata.version("endstone")
-
-
-def _get_minecraft_version():
-    version = Version(__version__)
-    major, minor, patch, build = [int(a) for a in version.release]
-    return f"{major}.{minor}.{patch}.{build:02}"
-
-
-__minecraft__version__ = _get_minecraft_version()
+__minecraft__version__ = (lambda v: f"{v[0]}.{v[1]}.{v[2]}.{v[3]:02}")([int(a) for a in Version(__version__).release])
