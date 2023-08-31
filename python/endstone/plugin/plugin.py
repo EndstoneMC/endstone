4311e8dad8e67ec26127a984af77fb62bb99c20f
Vincent <magicdroidx@gmail.com>
2023-08-31 14:18:40 +0100

    Update plugin.py to comply with the new plugin loader

--- python/endstone/plugin/plugin.py
+++ python/endstone/plugin/plugin.py
@@ -1,11 +1,12 @@
 from typing import final
 
+from endstone._plugin import PluginBase
+
 from endstone.command import Command, CommandSender
-from endstone.logger import Logger
 from .plugin_description import PluginDescription
 
 
-class Plugin:
+class Plugin(PluginBase):
     def on_load(self) -> None:
         pass
 
@@ -15,37 +16,17 @@ class Plugin:
     def on_disable(self) -> None:
         pass
 
+    def on_command(self, sender: CommandSender, command: Command, label: str, *args) -> bool:
+        return False
+
     # noinspection PyAttributeOutsideInit
     @final
     def _init(self, description: PluginDescription) -> None:
-        self._enabled = False
         self._description = description
-        self._logger = None  # Declaration only. Real value to be set by PythonPluginLoader::loadPlugin from C++
 
-    @final
     @property
     def description(self) -> PluginDescription:
         return self._description
 
-    @final
-    @property
-    def logger(self) -> Logger:
-        return self._logger
-
-    @final
-    @property
-    def enabled(self) -> bool:
-        return self._enabled
-
-    @final
-    def _set_enabled(self, enabled: bool) -> None:
-        if self._enabled is not enabled:
-            self._enabled = enabled
-
-            if self._enabled:
-                self.on_enable()
-            else:
-                self.on_disable()
-
-    def on_command(self, sender: CommandSender, command: Command, label: str, *args) -> bool:
-        return False
+    def _get_description(self):
+        return self.description
