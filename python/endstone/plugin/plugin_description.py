617da5a743b45038101bb37e08cc10735dc03920
Vincent <magicdroidx@gmail.com>
2023-09-01 12:23:20 +0100

    Rework parsing commands from plugin.toml

--- python/endstone/plugin/plugin_description.py
+++ python/endstone/plugin/plugin_description.py
@@ -20,18 +20,76 @@ class PluginDescriptionFile(PluginDescription):
             version = data.pop("version")
             main = data.pop("main")
             PluginDescription.__init__(self, name, version)
-            self._main: str = main
-            self._description: Optional[str] = None
-            self._authors: Optional[list[str]] = None
-            self._prefix: Optional[str] = None
-            self._commands: Optional[dict] = None
-
         except KeyError as e:
             raise RuntimeError(f"{e} is not defined")
 
+        self._main: str = main
+        self._description: Optional[str] = None
+        self._authors: Optional[list[str]] = None
+        self._prefix: Optional[str] = None
+        self._commands: list[Command] = []
+
         for k, v in data.items():
+            if k == "commands":
+                self._commands = self._parse_command_map(v)
+                continue
+
             if hasattr(self, f"_{k}"):
                 setattr(self, f"_{k}", v)
+
+    def _parse_command_map(self, command_map: dict) -> list[Command]:
+        commands = []
+        for name, value in command_map.items():
+            if ":" in name:
+                raise NameError(f"Could not load command {name} for plugin {self.name}: Illegal Characters")
+
+            command = Command(name)
+
+            description = value.get("description", None)
+            usages = value.get("usages", None)
+            aliases = value.get("aliases", None)
+
+            # TODO: permissions:
+            #  permission = value.get("permission", None)
+            #  permission_message = value.get("permission-message", None)
+
+            if description:
+                command.description = description
+
+            if usages:
+                if isinstance(usages, list):
+                    usage_list = usages
+                else:
+                    usage_list = [usages]
+
+                command.usages = usage_list
+
+            if aliases:
+                alias_list = []
+
+                if isinstance(aliases, list):
+                    for alias in aliases:
+                        if ":" in alias:
+                            raise NameError(f"Could not load alias {alias} for plugin {self.name}: Illegal Characters")
+
+                        alias_list.append(alias)
+                else:
+                    if ":" in aliases:
+                        raise NameError(f"Could not load alias {aliases} for plugin {self.name}: Illegal Characters")
+
+                    alias_list.append(aliases)
+
+                command.setAliases(alias_list)
+
+            # TODO: permission system
+            # if permission:
+            #     command.permission = permission
+            # if permission_message:
+            #     command.permission_message = permission_message
+
+            commands.append(command)
+
+        return commands
 
     @property
     def main(self) -> str:
@@ -51,17 +109,16 @@ class PluginDescriptionFile(PluginDescription):
 
     @property
     def commands(self) -> list[Command]:
-        # TODO: implement this
-        return []
+        return self._commands
 
-    def _get_description(self):
+    def _get_description(self) -> Optional[str]:
         return self.description
 
-    def _get_authors(self):
+    def _get_authors(self) -> Optional[list[str]]:
         return self.authors
 
-    def _get_prefix(self):
+    def _get_prefix(self) -> Optional[str]:
         return self.prefix
 
-    def _get_commands(self):
+    def _get_commands(self) -> list[Command]:
         return self.commands
--- src/pybind/_command.cpp
+++ src/pybind/_command.cpp
@@ -23,7 +23,11 @@ public:
 void export_command(py::module &m)
 {
     py::class_<Command, py::smart_holder>(m, "Command")
-        .def("execute", &Command::execute)
+        .def(py::init<const std::string &>(), py::arg("name"))
+        .def(py::init<const std::string &, const std::string &, const std::vector<std::string> &,
+                      const std::vector<std::string> &>(),
+             py::arg("name"), py::arg("description"), py::arg("usages"), py::arg("aliases"))
+        //        .def("execute", &Command::execute)
         .def_property("name", &Command::getName, &Command::setName)
         .def_property("label", &Command::getLabel, &Command::setLabel)
         .def_property("aliases", &Command::getAliases, &Command::setAliases)
@@ -31,19 +35,13 @@ void export_command(py::module &m)
         .def_property("usages", &Command::getUsages, &Command::setUsages)
         .def_property_readonly("registered", &Command::isRegistered)
         .def("register", &Command::registerTo, py::arg("command_map"))
-        .def("unregister", &Command::unregisterFrom, py::arg("command_map"))
-        .def("__repr__", [](const Command &c) {
-            return "<Command name='" + c.getName() + "' label='" + c.getLabel() + "'>";
-        });
+        .def("unregister", &Command::unregisterFrom, py::arg("command_map"));
 
     py::class_<CommandSender>(m, "CommandSender")
         .def("send_message", (void(CommandSender::*)(const std::string &) const) & CommandSender::sendMessage,
              py::arg("msg"))
         .def_property_readonly("server", &CommandSender::getServer)
-        .def_property_readonly("name", &CommandSender::getName)
-        .def("__repr__", [](const CommandSender &cs) {
-            return "<CommandSender name='" + std::string(cs.getName()) + "'>";
-        });
+        .def_property_readonly("name", &CommandSender::getName);
 
     py::class_<CommandExecutor, PyCommandExecutor>(m, "CommandExecutor")
         .def(py::init<>())
