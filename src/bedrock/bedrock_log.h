c6cdead9d6054f163cf7f3610f10a3ace7b947eb
Vincent <magicdroidx@gmail.com>
2023-09-05 23:45:49 +0100

    Add: hooks for BedrockLog

--- src/bedrock/bedrock_log.h
+++ src/bedrock/bedrock_log.h
@@ -97,9 +97,7 @@ enum class LogRule {
  * @param format Format string for the log message.
  * @param args Variable argument list.
  */
-void log_va(BedrockLog::LogCategory category, // always 0
-                        std::bitset<3> flags,             // always 1
-                        BedrockLog::LogRule rule,         // always 0
+BEDROCK_API void log_va(BedrockLog::LogCategory category, std::bitset<3> flags, BedrockLog::LogRule rule,
                         LogAreaID area, LogLevel level, char const *function, int line, char const *format,
                         va_list args);
 }; // namespace BedrockLog
