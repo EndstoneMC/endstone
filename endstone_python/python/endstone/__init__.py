757a44d820d55159964ad9ecf09746b2b9a51f0e
Vincent <magicdroidx@gmail.com>
2023-12-20 16:53:39 +0000

    feat: add endstone cd

--- endstone_python/python/endstone/__init__.py
+++ endstone_python/python/endstone/__init__.py
@@ -2,7 +2,7 @@ from importlib import import_module
 
 from packaging.version import Version
 
-from _version import __version__, __version_tuple__
+from ._version import __version__, __version_tuple__
 
 __minecraft__version__ = (lambda v: f"{v[0]}.{v[1]}.{v[2]}.{v[3]:02}")([int(a) for a in Version(__version__).release])
 
