9f8bbe26f854799157925cdf39921b305f65d818
Vincent <magicdroidx@gmail.com>
2023-12-22 02:05:09 +0000

    refactor: move internal classes to module endstone._internal

--- endstone_python/python/endstone/__init__.py
+++ endstone_python/python/endstone/__init__.py
@@ -1,5 +1,5 @@
 from endstone._api import Logger, Server
-from endstone._version import __version__
+from endstone._internal._version import __version__
 from packaging.version import Version
 
 __minecraft_version__ = (lambda v: f"{v[0]}.{v[1]}.{v[2]}.{v[3]:02}")([int(a) for a in Version(__version__).release])
