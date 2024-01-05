ce97489b9f40e3779ebf82cdc6c3b1a2b7cda3b9
Vincent <magicdroidx@gmail.com>
2024-01-05 16:37:00 +0000

    feat: add magic_enum package dependency

--- conanfile.py
+++ conanfile.py
@@ -97,6 +97,7 @@ class EndstoneRecipe(ConanFile):
         self.requires("spdlog/1.12.0")
         self.requires("fmt/[>=10.1.1]", transitive_headers=True, transitive_libs=True)
         self.requires("pybind11/2.11.1@pybind11/smart_holder", transitive_headers=True)
+        self.requires("magic_enum/0.9.5")
         if self.settings.os == "Windows":
             self.requires("minhook/1.3.3")
 
@@ -148,7 +149,7 @@ class EndstoneRecipe(ConanFile):
 
         self.cpp_info.components["runtime"].libs = []
         self.cpp_info.components["runtime"].set_property("cmake_target_name", "endstone::runtime")
-        self.cpp_info.components["runtime"].requires = ["api", "bedrock_internals", "core"]
+        self.cpp_info.components["runtime"].requires = ["api", "bedrock_internals", "core", "magic_enum::magic_enum"]
         if self.settings.os == "Windows":
             self.cpp_info.components["runtime"].requires.extend(["minhook::minhook"])
             self.cpp_info.components["runtime"].system_libs.extend(["dbghelp.lib"])
