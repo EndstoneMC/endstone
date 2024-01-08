37f59b2eca2b409e6514c1deb04048e82357c5bc
Vincent <magicdroidx@gmail.com>
2024-01-08 17:02:57 +0000

    feat: update funchook conanfile for MSVC support and add version 1.1.3 to CI

--- third_party/funchook/conanfile.py
+++ third_party/funchook/conanfile.py
@@ -4,6 +4,7 @@ from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get
+from conan.tools.microsoft import is_msvc
 
 required_conan_version = ">=1.53.0"
 
@@ -76,7 +77,10 @@ class PackageConan(ConanFile):
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = ["funchook"]
+        if is_msvc(self) and self.options.shared:
+            self.cpp_info.libs = ["funchook_dll"]
+        else:
+            self.cpp_info.libs = ["funchook"]
 
         self.cpp_info.set_property("cmake_file_name", "funchook")
         self.cpp_info.set_property("cmake_target_name", "funchook::funchook")
