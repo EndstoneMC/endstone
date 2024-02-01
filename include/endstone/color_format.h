0ed724a154a795d0eadc8a03991789096028b936
Vincent <magicdroidx@gmail.com>
2024-02-01 16:22:44 +0000

    feat(api): add ColorFormat

---
+++ include/endstone/color_format.h
@@ -0,0 +1,57 @@
+// Copyright (c) 2024, The Endstone Project. (https://endstone.dev) All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <string>
+
+struct ColorFormat {
+    // Escape
+    inline static const std::string ESCAPE = "§";
+
+    // Color code
+    inline static const std::string BLACK = ESCAPE + '0';
+    inline static const std::string DARK_BLUE = ESCAPE + '1';
+    inline static const std::string DARK_GREEN = ESCAPE + '2';
+    inline static const std::string DARK_AQUA = ESCAPE + '3';
+    inline static const std::string DARK_RED = ESCAPE + '4';
+    inline static const std::string DARK_PURPLE = ESCAPE + '5';
+    inline static const std::string GOLD = ESCAPE + '6';
+    inline static const std::string GRAY = ESCAPE + '7';
+    inline static const std::string DARK_GRAY = ESCAPE + '8';
+    inline static const std::string BLUE = ESCAPE + '9';
+    inline static const std::string GREEN = ESCAPE + 'a';
+    inline static const std::string AQUA = ESCAPE + 'b';
+    inline static const std::string RED = ESCAPE + 'c';
+    inline static const std::string LIGHT_PURPLE = ESCAPE + 'd';
+    inline static const std::string YELLOW = ESCAPE + 'e';
+    inline static const std::string WHITE = ESCAPE + 'f';
+    inline static const std::string MINECOIN_GOLD = ESCAPE + 'g';
+    inline static const std::string MATERIAL_QUARTZ = ESCAPE + 'h';
+    inline static const std::string MATERIAL_IRON = ESCAPE + 'i';
+    inline static const std::string MATERIAL_NETHERITE = ESCAPE + 'j';
+    inline static const std::string MATERIAL_REDSTONE = ESCAPE + 'm';
+    inline static const std::string MATERIAL_COPPER = ESCAPE + 'n';
+    inline static const std::string MATERIAL_GOLD = ESCAPE + 'p';
+    inline static const std::string MATERIAL_EMERALD = ESCAPE + 'q';
+    inline static const std::string MATERIAL_DIAMOND = ESCAPE + 's';
+    inline static const std::string MATERIAL_LAPIS = ESCAPE + 't';
+    inline static const std::string MATERIAL_AMETHYST = ESCAPE + 'u';
+
+    // Format code
+    inline static const std::string OBFUSCATED = ESCAPE + 'k';
+    inline static const std::string BOLD = ESCAPE + 'l';
+    inline static const std::string ITALIC = ESCAPE + 'o';
+    inline static const std::string RESET = ESCAPE + 'r';
+};
---
+++ src/endstone_python/color_format.cpp
@@ -0,0 +1,56 @@
+// Copyright (c) 2024, The Endstone Project. (https://endstone.dev) All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "endstone/color_format.h"
+
+#include "endstone_python/endstone_python.h"
+
+#define ADD_COLOR_FORMAT(Name) def_property_readonly_static(#Name, [](const py::object &) { return ColorFormat::Name; })
+
+void def_color_format(py::module &m)
+{
+    py::class_<ColorFormat>(m, "ColorFormat")  //
+        .ADD_COLOR_FORMAT(BLACK)
+        .ADD_COLOR_FORMAT(DARK_BLUE)
+        .ADD_COLOR_FORMAT(DARK_GREEN)
+        .ADD_COLOR_FORMAT(DARK_AQUA)
+        .ADD_COLOR_FORMAT(DARK_RED)
+        .ADD_COLOR_FORMAT(DARK_PURPLE)
+        .ADD_COLOR_FORMAT(GOLD)
+        .ADD_COLOR_FORMAT(GRAY)
+        .ADD_COLOR_FORMAT(DARK_GRAY)
+        .ADD_COLOR_FORMAT(BLUE)
+        .ADD_COLOR_FORMAT(GREEN)
+        .ADD_COLOR_FORMAT(AQUA)
+        .ADD_COLOR_FORMAT(RED)
+        .ADD_COLOR_FORMAT(LIGHT_PURPLE)
+        .ADD_COLOR_FORMAT(YELLOW)
+        .ADD_COLOR_FORMAT(WHITE)
+        .ADD_COLOR_FORMAT(MINECOIN_GOLD)
+        .ADD_COLOR_FORMAT(MATERIAL_QUARTZ)
+        .ADD_COLOR_FORMAT(MATERIAL_IRON)
+        .ADD_COLOR_FORMAT(MATERIAL_NETHERITE)
+        .ADD_COLOR_FORMAT(MATERIAL_REDSTONE)
+        .ADD_COLOR_FORMAT(MATERIAL_COPPER)
+        .ADD_COLOR_FORMAT(MATERIAL_GOLD)
+        .ADD_COLOR_FORMAT(MATERIAL_EMERALD)
+        .ADD_COLOR_FORMAT(MATERIAL_DIAMOND)
+        .ADD_COLOR_FORMAT(MATERIAL_LAPIS)
+        .ADD_COLOR_FORMAT(MATERIAL_AMETHYST)
+
+        .ADD_COLOR_FORMAT(OBFUSCATED)
+        .ADD_COLOR_FORMAT(BOLD)
+        .ADD_COLOR_FORMAT(ITALIC)
+        .ADD_COLOR_FORMAT(RESET);
+}
