812ab167e0b97edf22a8dd496e4ff9912616f6d3
Vincent <magicdroidx@gmail.com>
2024-03-02 19:30:04 +0000

    refactor: corrected a typo in the include path

--- src/endstone_core/spdlog/text_formatter.cpp
+++ src/endstone_core/spdlog/text_formatter.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "endstone/detail//spdlog/text_formatter.h"
+#include "endstone/detail/spdlog/text_formatter.h"
 
 #include <spdlog/details/fmt_helper.h>
 
