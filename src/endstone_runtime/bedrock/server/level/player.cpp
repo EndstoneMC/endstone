3defe46d8e1c2bface14c2a6f1f488f0657facb6
Vincent <magicdroidx@gmail.com>
2024-03-15 16:22:05 +0000

    feat: hide or unhide commands when Player's permission level changed

--- src/endstone_runtime/bedrock/server/level/player.cpp
+++ src/endstone_runtime/bedrock/server/level/player.cpp
@@ -19,17 +19,56 @@
 #include "endstone/detail/server.h"
 #include "endstone/detail/singleton.h"
 
-void Player::setPermissions(CommandPermissionLevel level)
+namespace {
+
+class DummyPlayerSender : public endstone::detail::BaseCommandSender {
+public:
+    explicit DummyPlayerSender(CommandPermissionLevel level) : level_(level) {}
+    [[nodiscard]] bool isOp() const override
+    {
+        return level_ != CommandPermissionLevel::Any;
+    }
+    void setOp(bool value) override {}
+    void sendMessage(const std::string &message) const override {}
+    void sendErrorMessage(const std::string &message) const override {}
+    [[nodiscard]] std::string getName() const override
+    {
+        return "";
+    }
+
+private:
+    CommandPermissionLevel level_;
+};
+
+// TODO: refactor this into endstone::Player::sendCommands or endstone::Server::syncCommands
+void sendCommands(Player &player, CommandPermissionLevel level)
 {
     using endstone::detail::EndstoneServer;
     using endstone::detail::Singleton;
-
-    ENDSTONE_HOOK_CALL_ORIGINAL(&Player::setPermissions, this, level);
-    // TODO: refactor this into endstone::Player::sendCommands or endstone::Server::syncCommands
-    // TODO: set command flags (hidden) based on testPermissionSilently results
     auto &server = Singleton<EndstoneServer>::getInstance();
     auto &registry = server.getMinecraftCommands().getRegistry();
+
     AvailableCommandsPacket packet = registry.serializeAvailableCommands();
-    // TODO: use virtual function Player::sendNetworkPacket instead of this
-    getPacketSender().send(packet);
+
+    auto &command_map = server.getCommandMap();
+    // TODO: use endstone::Player instead
+    auto perm = DummyPlayerSender(level);
+    for (auto &data : packet.commands) {
+        auto name = data.name;
+        auto *command = command_map.getCommand(name);
+        if (command && command->isRegistered() && command->testPermissionSilently(perm)) {
+            continue;
+        }
+        data.command_flag |= (CommandFlag::HIDDEN_FROM_PLAYER | CommandFlag::HIDDEN_FROM_BLOCK);
+    }
+
+    // TODO: use virtual function Player::sendNetworkPacket instead
+    player.getPacketSender().send(packet);
+}
+}  // namespace
+
+void Player::setPermissions(CommandPermissionLevel level)
+{
+    ENDSTONE_HOOK_CALL_ORIGINAL(&Player::setPermissions, this, level);
+    sendCommands(*this, level);
 }
