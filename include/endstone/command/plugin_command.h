fc6cc150f05c65e73e09dd0dac841e86cb73e07f
Vincent <magicdroidx@gmail.com>
2024-03-19 23:23:00 +0000

    refactor: move PluginCommand to separate header file

---
+++ include/endstone/command/plugin_command.h
@@ -0,0 +1,98 @@
+// Copyright (c) 2024, The Endstone Project. (https://endstone.dev) All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include "endstone/command/command.h"
+#include "endstone/command/command_executor.h"
+#include "endstone/plugin/plugin.h"
+
+namespace endstone {
+
+class PluginCommand : public Command {
+public:
+    PluginCommand(Plugin &owner, std::string name, std::string description = "", std::vector<std::string> usages = {},
+                  std::vector<std::string> aliases = {}, std::vector<std::string> permissions = {})
+        : Command(std::move(name), std::move(description), std::move(usages), std::move(aliases),
+                  std::move(permissions)),
+          owner_(owner)
+    {
+    }
+
+    bool execute(CommandSender &sender, const std::vector<std::string> &args) const override
+    {
+        if (!owner_.isEnabled()) {
+            sender.sendMessage("Cannot execute command '{}' in plugin {}. Plugin is disabled.", getName(),
+                               getPlugin().getDescription().getFullName());
+            return false;
+        }
+
+        if (!testPermission(sender)) {
+            return true;
+        }
+
+        try {
+            return getExecutor().onCommand(sender, *this, args);
+        }
+        catch (std::exception &e) {
+            getPlugin().getLogger().error("Unhandled exception executing command '{}' in plugin {}", getName(),
+                                          owner_.getDescription().getFullName());
+            getPlugin().getLogger().error(e.what());
+            return false;
+        }
+    }
+
+    /**
+     * Sets the CommandExecutor to run when parsing this command
+     *
+     * @param executor New executor to run
+     */
+    virtual void setExecutor(std::shared_ptr<CommandExecutor> executor)
+    {
+        executor_ = std::move(executor);
+    }
+
+    /**
+     * Gets the CommandExecutor associated with this command
+     *
+     * @return CommandExecutor object linked to this command
+     */
+    [[nodiscard]] virtual CommandExecutor &getExecutor() const
+    {
+        if (executor_) {
+            return *executor_;
+        }
+        return owner_;
+    }
+
+    /**
+     * Gets the owner of this PluginCommand
+     *
+     * @return Plugin that owns this command
+     */
+    [[maybe_unused]] [[nodiscard]] Plugin &getPlugin() const
+    {
+        return owner_;
+    }
+
+    [[nodiscard]] PluginCommand *asPluginCommand() override
+    {
+        return this;
+    }
+
+private:
+    Plugin &owner_;
+    std::shared_ptr<CommandExecutor> executor_;
+};
+}  // namespace endstone
