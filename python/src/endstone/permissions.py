d7e8726e35dd5601b0e67a2a4ee379aff98e0056
Vincent <magicdroidx@gmail.com>
2024-03-21 02:09:11 +0000

    feat: add permissions module

---
+++ python/src/endstone/permissions.py
@@ -0,0 +1,9 @@
+from endstone._internal.endstone_python import (
+    Permission,
+    Permissible,
+    PermissionAttachment,
+    PermissionAttachmentInfo,
+    PermissionDefault,
+)
+
+__all__ = ["Permission", "Permissible", "PermissionAttachment", "PermissionAttachmentInfo", "PermissionDefault"]
