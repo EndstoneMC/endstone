c5a5a1aa8487bf796619ec337a80e20a3bd98fd8
Vincent <magicdroidx@gmail.com>
2024-03-23 12:58:35 +0000

    feat: add event_handler decorator

--- python/src/endstone/event.py
+++ python/src/endstone/event.py
@@ -1,3 +1,16 @@
 from endstone._internal.endstone_python import Event, EventPriority, ServerLoadEvent
 
-__all__ = ["Event", "EventPriority", "ServerLoadEvent"]
+__all__ = ["Event", "EventPriority", "ServerLoadEvent", "event_handler"]
+
+
+def event_handler(func=None, *, priority: EventPriority = EventPriority.NORMAL, ignore_cancelled: bool = False):
+    def decorator(f):
+        setattr(f, "_is_event_handler", True)
+        setattr(f, "_priority", priority)
+        setattr(f, "_ignore_cancelled", ignore_cancelled)
+        return f
+
+    if func:
+        return decorator(func)
+
+    return decorator
