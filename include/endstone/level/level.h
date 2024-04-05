484f6066161b81ab6a63f726b84ab3aa3dd47023
Vincent <magicdroidx@gmail.com>
2024-04-05 11:02:56 +0100

    feat: add Level and EndstoneLevel class

---
+++ include/endstone/level/level.h
@@ -0,0 +1,47 @@
+// Copyright (c) 2024, The Endstone Project. (https://endstone.dev) All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <string>
+
+namespace endstone {
+
+class Level {
+public:
+    virtual ~Level() = default;
+
+    /**
+     * Gets the unique name of this level
+     *
+     * @return Name of this level
+     */
+    [[nodiscard]] virtual std::string getName() const = 0;
+
+    /**
+     * Gets the relative in-game time of this level.
+     *
+     * @return The current relative time
+     */
+    [[nodiscard]] virtual int getTime() const = 0;
+
+    /**
+     * Sets the relative in-game time on the server.
+     *
+     * @param time The new relative time to set the in-game time to
+     */
+    virtual void setTime(int time) = 0;
+};
+
+}  // namespace endstone
---
+++ src/endstone_core/level/level.cpp
@@ -0,0 +1,36 @@
+// Copyright (c) 2024, The Endstone Project. (https://endstone.dev) All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "endstone/detail/level/level.h"
+
+namespace endstone::detail {
+
+EndstoneLevel::EndstoneLevel(BedrockLevel &level) : level_(level) {}
+
+std::string EndstoneLevel::getName() const
+{
+    return level_.getLevelId();
+}
+
+int EndstoneLevel::getTime() const
+{
+    return level_.getTime();
+}
+
+void EndstoneLevel::setTime(int time)
+{
+    level_.setTime(time);
+}
+
+};  // namespace endstone::detail
