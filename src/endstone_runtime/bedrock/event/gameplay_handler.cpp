bab0df1575792e1ad9a22e1a41f94d3ac90b699e
Vincent <magicdroidx@gmail.com>
2024-04-07 00:56:36 +0100

    style: clean up code

--- src/endstone_runtime/bedrock/event/gameplay_handler.cpp
+++ src/endstone_runtime/bedrock/event/gameplay_handler.cpp
@@ -28,60 +28,98 @@ using endstone::detail::EndstoneServer;
 using endstone::detail::VirtualTable;
 
 namespace endstone::detail {
+
+class EndstoneLevelGameplayHandler : public ::LevelGameplayHandler {
+
+    static Level &getLevel(LevelGameplayHandler &handler_)
+    {
+        // Find out the level where this event is occurring
+        auto &server = entt::locator<EndstoneServer>::value();
+        auto levels = server.getLevels();
+        auto it = std::find_if(begin(levels), end(levels), [&handler_](auto *level) {
+            return &(static_cast<EndstoneLevel *>(level)
+                         ->getBedrockLevel()
+                         .getLevelEventCoordinator()
+                         .getLevelGameplayHandler()) == &handler_;
+        });
+        if (it == levels.end()) {
+            server.getLogger().critical("Unable to find level associated with the provided LevelGameplayHandler");
+            std::terminate();
+        }
+        auto &level = **it;
+        return level;
+    }
+
+public:
+    GameplayHandlerResult<CoordinatorResult> handleEvent(LevelWeatherChangedEvent &event)
+    {
+        auto &vtable = entt::locator<VirtualTable<ScriptLevelGameplayHandler>>::value();
+
+#ifdef _WIN32
+        GameplayHandlerResult<CoordinatorResult> result;
+        vtable.call<3, GameplayHandlerResult<CoordinatorResult>>(this, &result, event);
+#else
+        auto result = vtable.call<3, GameplayHandlerResult<CoordinatorResult>>(this, event);
+#endif
+
+        if (result.value == CoordinatorResult::Deny || result.result == EventResult::Deny) {
+            return result;
+        }
+
+        auto &server = entt::locator<EndstoneServer>::value();
+        auto &level = getLevel(*this);
+
+        if (event.from_rain != event.to_rain) {
+            server.getLogger().debug("Weather state was changed in level: {}", level.getName());
+            endstone::WeatherChangeEvent e(level, event.to_rain);
+            server.getPluginManager().callEvent(e);
+            if (e.isCancelled()) {
+                event.to_rain = event.from_rain;
+            }
+        }
+
+        if (event.from_lightning != event.to_lightning) {
+            server.getLogger().debug("Thunder state was changed in level: {}", level.getName());
+            endstone::ThunderChangeEvent e(level, event.to_lightning);
+            server.getPluginManager().callEvent(e);
+            if (e.isCancelled()) {
+                event.to_lightning = event.from_lightning;
+            }
+        }
+
+        return result;
+    }
+
+    HandlerResult handleEvent(LevelStartLeaveGameEvent const &event)
+    {
+        auto &server = entt::locator<EndstoneServer>::value();
+        auto &level = getLevel(*this);
+        server.getLogger().debug("Unloading level: {}. Game is being shut down.", level.getName());
+        auto &vtable = entt::locator<VirtualTable<ScriptLevelGameplayHandler>>::value();
+        return vtable.call<6, HandlerResult>(this, event);
+    }
+
+    HandlerResult handleEvent(LevelAddedActorEvent const &event)
+    {
+        auto &server = entt::locator<EndstoneServer>::value();
+        auto &level = getLevel(*this);
+        server.getLogger().debug("An actor has been added to level: {}.", level.getName());
+        auto &vtable = entt::locator<VirtualTable<ScriptLevelGameplayHandler>>::value();
+        return vtable.call<10, HandlerResult>(this, event);
+    }
+};
+
 template <>
 void VirtualTableHook<ScriptLevelGameplayHandler>::init()
 {
     hook<3, LevelWeatherChangedEvent &>(&EndstoneLevelGameplayHandler::handleEvent);
+    // 4: handleEvent(ScriptingInitializeEvent const&)
+    // 5: handleEvent(LevelGameRuleChangeEvent const&)
+    hook<6, LevelStartLeaveGameEvent const &>(&EndstoneLevelGameplayHandler::handleEvent);
+    // 7: handleEvent(LevelDayCycleEvent const&)
+    // 8: handleEvent(LevelSoundBroadcastEvent const&)
+    // 9: handleEvent(LevelBroadcastEvent const&)
+    hook<10, LevelAddedActorEvent const &>(&EndstoneLevelGameplayHandler::handleEvent);
 }
+
 }  // namespace endstone::detail
-
-GameplayHandlerResult<CoordinatorResult> EndstoneLevelGameplayHandler::handleEvent(LevelWeatherChangedEvent &event)
-{
-    auto &vtable = entt::locator<VirtualTable<ScriptLevelGameplayHandler>>::value();
-
-#ifdef _WIN32
-    GameplayHandlerResult<CoordinatorResult> result;
-    vtable.call<3, GameplayHandlerResult<CoordinatorResult>>(this, &result, event);
-#else
-    auto result = vtable.call<3, GameplayHandlerResult<CoordinatorResult>>(this, event);
-#endif
-
-    if (result.value == CoordinatorResult::Deny || result.result == EventResult::Deny) {
-        return result;
-    }
-
-    // Find out the level where this event is occurring
-    auto &server = entt::locator<EndstoneServer>::value();
-    auto levels = server.getLevels();
-    auto it = std::find_if(begin(levels), end(levels), [this](auto *level) {
-        return &(static_cast<EndstoneLevel *>(level)
-                     ->getBedrockLevel()
-                     .getLevelEventCoordinator()
-                     .getLevelGameplayHandler()) == this;
-    });
-    if (it == levels.end()) {
-        server.getLogger().critical("Event {} called on unknown level.", typeid(event).name());
-        std::terminate();
-    }
-    auto &level = **it;
-
-    if (event.from_rain != event.to_rain) {
-        server.getLogger().debug("WeatherChangeEvent was called on level: {}", level.getName());
-        endstone::WeatherChangeEvent e(level, event.to_rain);
-        server.getPluginManager().callEvent(e);
-        if (e.isCancelled()) {
-            event.to_rain = event.from_rain;
-        }
-    }
-
-    if (event.from_lightning != event.to_lightning) {
-        server.getLogger().debug("ThunderChangeEvent was called on level: {}", level.getName());
-        endstone::ThunderChangeEvent e(level, event.to_lightning);
-        server.getPluginManager().callEvent(e);
-        if (e.isCancelled()) {
-            event.to_lightning = event.from_lightning;
-        }
-    }
-
-    return result;
-}
