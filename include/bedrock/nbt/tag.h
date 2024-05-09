60c3c9aa345d72a2c409e34de54c80478edd08a5
Vincent <magicdroidx@gmail.com>
2024-05-09 17:22:34 +0100

    feat: add Tag::Type

---
+++ include/bedrock/nbt/tag.h
@@ -0,0 +1,34 @@
+// Copyright (c) 2024, The Endstone Project. (https://endstone.dev) All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+class Tag {
+public:
+    enum class Type {
+        End = 0,
+        Byte = 1,
+        Short = 2,
+        Int = 3,
+        Int64 = 4,
+        Float = 5,
+        Double = 6,
+        ByteArray = 7,
+        String = 8,
+        List = 9,
+        Compound = 10,
+        IntArray = 11,
+        NumTagTypes = 12,
+    };
+};
