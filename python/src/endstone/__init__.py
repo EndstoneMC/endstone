2da6ac5030f44814e6b0fb7cea61f745e2c33f4e
Vincent <magicdroidx@gmail.com>
2024-06-13 18:43:51 +0100

    feat: update minecraft version to 1.21.0

--- python/src/endstone/__init__.py
+++ python/src/endstone/__init__.py
@@ -1,6 +1,6 @@
 from endstone._internal.endstone_python import ColorFormat, GameMode, Logger, Player, Server
 from endstone._internal.version import __version__
 
-__minecraft_version__ = "1.20.81"
+__minecraft_version__ = "1.21.0"
 
 __all__ = ["__version__", "__minecraft_version__", "ColorFormat", "GameMode", "Logger", "Player", "Server"]
