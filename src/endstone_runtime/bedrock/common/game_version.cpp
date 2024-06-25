0bc48f0851cdb51318d7de52a2841fd7b7a86309
Vincent <magicdroidx@gmail.com>
2024-06-25 10:20:44 +0100

    refactor: remove Common::getGameVersionString to file bedrock/common/game_version.h

--- src/endstone_runtime/bedrock/common/game_version.cpp
+++ src/endstone_runtime/bedrock/common/game_version.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "bedrock/common.h"
+#include "bedrock/common/game_version.h"
 
 #include "endstone/detail/hook.h"
 
