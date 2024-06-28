499369dbde0a46077dcb66153a233db76cc49d44
Vincent <magicdroidx@gmail.com>
2024-06-28 17:29:23 +0100

    feat: expand Item class

--- include/bedrock/world/item/item.h
+++ include/bedrock/world/item/item.h
@@ -16,10 +16,57 @@
 
 #include <string>
 
+#include "bedrock/common/game_version.h"
+#include "bedrock/world/item/item_tag.h"
+#include "bedrock/world/level/block/block_legacy.h"
+
 class Item {
 public:
     virtual ~Item() = 0;
 
+    [[nodiscard]] std::int8_t getMaxStackSize() const
+    {
+        return max_stack_size_;
+    }
+
+    [[nodiscard]] std::int16_t getId() const
+    {
+        return id_;
+    }
+
+    [[nodiscard]] std::int16_t getMaxDamage() const
+    {
+        return max_damage_;
+    }
+
 private:
-    std::string texture_atlas_file_;  // +8
+    std::string texture_atlas_file_;                  // +8
+    int frame_count_;                                 // +40
+    std::uint16_t use_anim_;                          // +44
+    bool animates_in_toolbar_;                        // +46
+    std::string hover_text_color_format_;             // +48
+    std::uint64_t unknown_;                           // +80
+    int atlas_total_frame_;                           // +88
+    std::string icon_name_;                           // +96
+    std::string atlas_name_;                          // +128
+    std::int8_t max_stack_size_;                      // +160
+    std::int16_t id_;                                 // +162
+    std::string description_id_;                      // +168
+    HashedString raw_name_id_;                        // +200
+    std::string namespace_;                           // +248
+    HashedString full_name_;                          // +280
+    std::int16_t max_damage_;                         // +328
+    std::int16_t unknown2_;                           // +330
+    int max_use_duration_;                            // +332
+    BaseGameVersion min_required_base_game_version_;  // +336
+    WeakPtr<BlockLegacy> legacy_block_;               // +456
+    CreativeItemCategory creative_category_;          // +464
+    Item *crafting_remaining_item_;                   // +472
+    std::string creative_group_;                      // +480
+    std::uint64_t unknown3_;                          // +512
+    int unknown4_;                                    // +520
+    int unknown5_;                                    // +524
+    std::vector<void *> unknown6_;                    // +528
+    std::vector<void *> unknown7_;                    // +552
+    std::vector<ItemTag> tags_;                       // +576
 };
