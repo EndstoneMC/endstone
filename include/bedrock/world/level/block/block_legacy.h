e1cc60cf78ead1d667467557ef554ee03850843a
Vincent <magicdroidx@gmail.com>
2024-06-30 15:14:47 +0100

    fix: size of BlockLegacy on Linux

--- include/bedrock/world/level/block/block_legacy.h
+++ include/bedrock/world/level/block/block_legacy.h
@@ -301,12 +301,12 @@ private:
     std::string description_id_;                                      // +40
     NameInfo name_info_;                                              // +72  (+64)
     BlockProperty properties_;                                        // +248 (+208)
-    bool fancy_;                                                      // +256
+    bool fancy_;                                                      // +256 (+216)
     BlockRenderLayer render_layer_;                                   // +260 (+220)
-    bool render_layer_can_render_as_opaque_;                          // +264
+    bool render_layer_can_render_as_opaque_;                          // +264 (+224)
     BlockActorType block_entity_type_;                                // +268 (+228)
-    bool animated_texture_;                                           // +272
-    float brightness_gamma_;                                          // +276
+    bool animated_texture_;                                           // +272 (+232)
+    float brightness_gamma_;                                          // +276 (+236)
     float thickness_;                                                 // +280 (+240)
     bool can_slide_;                                                  // +284 (+244)
     bool can_react_to_neighbors_during_instatick_;                    // +285 (+245)
@@ -316,7 +316,7 @@ private:
     bool heavy_;                                                      // +304 (+264)
     float particle_quantity_scalar_;                                  // +308 (+268)
     CreativeItemCategory creative_item_category_;                     // +312 (+272)
-    std::string creative_group_;                                      // +320
+    std::string creative_group_;                                      // +320 (+280)
     bool is_hidden_in_commands_;                                      // +352 (+304)
     bool allow_runes_;                                                // +353 (+305)
     bool can_be_broken_from_falling_;                                 // +354 (+306)
@@ -324,13 +324,13 @@ private:
     bool solid_;                                                      // +356 (+308)
     bool pushes_out_items_;                                           // +357 (+309)
     bool ignore_block_for_inside_cube_renderer_;                      // +358 (+310)
-    bool is_trapdoor_;                                                // +359
-    bool is_door_;                                                    // +360
+    bool is_trapdoor_;                                                // +359 (+311)
+    bool is_door_;                                                    // +360 (+312)
     bool is_opaque_full_block_;                                       // +361 (+313)
     float translucency_;                                              // +364 (+316)
     bool should_random_tick_;                                         // +368 (+320)
     bool should_random_tick_extra_layer_;                             // +369 (+321)
-    bool is_mob_piece_;                                               // +370
+    bool is_mob_piece_;                                               // +370 (+322)
     bool can_be_extra_block_;                                         // +371 (+323)
     bool can_propagate_brightness_;                                   // +372 (+324)
     Brightness light_block_;                                          // +373 (+325)
@@ -343,7 +343,7 @@ private:
     mce::Color map_color_;                                            // +396 (+348)
     float friction_;                                                  // +412 (+364)
     BlockTintType block_tint_type_;                                   // +416 (+368)
-    bool unknown_;                                                    // +420
+    bool unknown_;                                                    // +420 (+372)
     NewBlockID id_;                                                   // +422 (+374)
     BaseGameVersion min_required_game_version_;                       // +424 (+376)
     bool is_vanilla_;                                                 // +544 (+472)
@@ -360,8 +360,9 @@ private:
     std::vector<void *> unknown4_;                                    // +800 (+688)
 #ifdef __linux__                                                      //
     std::shared_timed_mutex mutex_;                                   //      (+712)
+#else                                                                 //
+    std::int64_t unknown5_;  // +824
 #endif                                                                //
-    std::int64_t unknown5_;                                           // +824 (+848)
     std::unordered_map<void *, void *> unknown6_;                     // +832 (+856)
     std::unique_ptr<void *> block_state_group_;                       // +896        void* = BlockStateGroup
     std::unique_ptr<void *> resource_drops_strategy_;                 // +904        void* = IResourceDropsStrategy
@@ -370,5 +371,4 @@ private:
     std::vector<void *> unknown9_;                                    // +928
     std::vector<void *> unknown10_;                                   // +952
     // ...
-    // TODO(fixme): check offsets on Linux
 };
