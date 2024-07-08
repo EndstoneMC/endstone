7dfd10b27c212b76043c718771b7850974fc7d72
Vincent <magicdroidx@gmail.com>
2024-07-08 22:17:04 +0100

    feat: add base64_decode

--- include/endstone/detail/base64.h
+++ include/endstone/detail/base64.h
@@ -19,6 +19,8 @@
 #include <string>
 #include <string_view>
 
+#include <nonstd/expected.hpp>
+
 namespace endstone::detail {
 
 template <class OutputBuffer, class InputIterator>
@@ -47,6 +49,55 @@ inline OutputBuffer base64_encode(std::string_view data)
 inline std::string base64_encode(std::string_view data)
 {
     return base64_encode<std::string>(std::begin(data), std::end(data));
+}
+
+template <class OutputBuffer>
+inline std::optional<OutputBuffer> base64_decode(std::string_view data)
+{
+    typedef typename OutputBuffer::value_type output_value_type;
+    static_assert(std::is_same_v<output_value_type, char> || std::is_same_v<output_value_type, signed char> ||
+                  std::is_same_v<output_value_type, unsigned char> || std::is_same_v<output_value_type, std::byte>);
+
+    static OutputBuffer empty_buffer;
+    if (data.empty()) {
+        return empty_buffer;
+    }
+
+    if ((data.size() & 3) != 0) {
+        // Invalid base64 encoded data - Size not divisible by 4
+        return std::nullopt;
+    }
+
+    const size_t num_padding = std::count(data.rbegin(), data.rbegin() + 4, '=');
+    if (num_padding > 2) {
+        // Invalid base64 encoded data - Found more than 2 padding signs
+        return std::nullopt;
+    }
+
+    const size_t buffer_size = (data.size() * 3 >> 2) - num_padding;
+    OutputBuffer decoded(buffer_size, '.');
+    const auto *bytes = data.data();
+    char *out = reinterpret_cast<char *>(&decoded[0]);
+    std::size_t decoded_size;
+    if (::base64_decode(bytes, data.size(), out, &decoded_size, 0)) {
+        return decoded;
+    }
+    return std::nullopt;
+}
+
+template <class OutputBuffer, class InputIterator>
+inline std::optional<OutputBuffer> base64_decode(InputIterator begin, InputIterator end)
+{
+    typedef std::decay_t<decltype(*begin)> input_value_type;
+    static_assert(std::is_same_v<input_value_type, char> || std::is_same_v<input_value_type, signed char> ||
+                  std::is_same_v<input_value_type, unsigned char> || std::is_same_v<input_value_type, std::byte>);
+    std::string_view data(reinterpret_cast<const char *>(&*begin), end - begin);
+    return base64_decode<OutputBuffer>(data);
+}
+
+inline std::optional<std::string> base64_decode(std::string_view data)
+{
+    return base64_decode<std::string>(data);
 }
 
 }  // namespace endstone::detail
--- tests/test_base64.cpp
+++ tests/test_base64.cpp
@@ -20,7 +20,6 @@
 #include <gtest/gtest.h>
 
 #include "endstone/detail/base64.h"
-
 // NOLINTNEXTLINE
 TEST(Base64Encode, EncodesEmpty)
 {
@@ -119,6 +118,207 @@ TEST(Base64Encode, EncodesBrownFastFoxNullInMiddle)
 }
 
 // NOLINTNEXTLINE
+TEST(Base64Decode, FailDecodeOneString)
+{
+    std::string const input{"1"};
+    ASSERT_FALSE(endstone::detail::base64_decode(input).has_value());
+}
+
+// NOLINTNEXTLINE
+TEST(Base64Decode, FailDecodeOneStringPadded)
+{
+    std::string const input{"1==="};
+    ASSERT_FALSE(endstone::detail::base64_decode(input).has_value());
+}
+
+// NOLINTNEXTLINE
+TEST(Base64Decode, FailDecodeOneCharRemaining)
+{
+    std::string const input{"something"};
+    ASSERT_FALSE(endstone::detail::base64_decode(input).has_value());
+}
+
+// NOLINTNEXTLINE
+TEST(Base64Decode, FailDecodeNonSize4Bigger)
+{
+    std::string const input{"SomethingEntirelyDifferent"};
+    ASSERT_FALSE(endstone::detail::base64_decode(input).has_value());
+}
+
+// NOLINTNEXTLINE
+TEST(Base64Decode, FailDecodeNonBase64Short)
+{
+    std::string const input{"a aa"};
+    ASSERT_FALSE(endstone::detail::base64_decode(input).has_value());
+}
+
+// NOLINTNEXTLINE
+TEST(Base64Decode, FailDecodeNonBase64Longer)
+{
+    std::string const input{"aaa`aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"};
+    ASSERT_FALSE(endstone::detail::base64_decode(input).has_value());
+}
+
+// NOLINTNEXTLINE
+TEST(Base64Decode, DecodesMissingTwoPads0)
+{
+    std::string const input{"12"};
+    ASSERT_FALSE(endstone::detail::base64_decode(input).has_value());
+}
+
+// NOLINTNEXTLINE
+TEST(Base64Decode, DecodesMissingTwoPads1)
+{
+    std::string const input = "AA";
+    ASSERT_FALSE(endstone::detail::base64_decode(input).has_value());
+}
+
+// NOLINTNEXTLINE
+TEST(Base64Decode, DecodesMissingOnePad0)
+{
+    std::string const input = "AAA";
+    ASSERT_FALSE(endstone::detail::base64_decode(input).has_value());
+}
+
+// NOLINTNEXTLINE
+TEST(Base64Decode, DecodesMissingOnePad1)
+{
+    std::string const input{"12a"};
+    ASSERT_FALSE(endstone::detail::base64_decode(input).has_value());
+}
+
+// NOLINTNEXTLINE
+TEST(Base64Decode, DecodesMissingIssueExample)
+{
+    std::string const input = "eyJuYW1lIjoiSm9obiBEb2UifQ";
+    ASSERT_FALSE(endstone::detail::base64_decode(input).has_value());
+}
+
+// NOLINTNEXTLINE
+TEST(Base64Decode, DecodesEmptyString)
+{
+    std::string const input{};
+    std::string expected{};
+    auto const actual{endstone::detail::base64_decode("")};
+
+    ASSERT_EQ(expected, actual);
+}
+
+// NOLINTNEXTLINE
+TEST(Base64Decode, DecodesZeroArray)
+{
+    std::string const input{"AAAA"};
+    std::vector<std::uint8_t> const expected{0x00, 0x00, 0x00};
+    auto const actual{endstone::detail::base64_decode<std::vector<std::uint8_t>>(input)};
+
+    ASSERT_EQ(actual, expected);
+}
+
+// NOLINTNEXTLINE
+TEST(Base64Decode, DecodesZeroArrayTwice)
+{
+    std::string const input{"AAAAAAAA"};
+    std::vector<std::uint8_t> const expected{0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+    auto const actual{endstone::detail::base64_decode<std::vector<std::uint8_t>>(input)};
+
+    ASSERT_EQ(actual, expected);
+}
+
+// NOLINTNEXTLINE
+TEST(Base64Decode, DecodesZeroArrayOneByte)
+{
+    std::string const input{"AA=="};
+    std::vector<std::uint8_t> const expected{0x00};
+    auto const actual{endstone::detail::base64_decode<std::vector<std::uint8_t>>(input)};
+
+    ASSERT_EQ(actual, expected);
+}
+
+// NOLINTNEXTLINE
+TEST(Base64Decode, DecodesZeroArrayTwoBytes)
+{
+    std::string const input{"AAA="};
+    std::vector<std::uint8_t> const expected{0x00, 0x00};
+    auto const actual{endstone::detail::base64_decode<std::vector<std::uint8_t>>(input)};
+
+    ASSERT_EQ(actual, expected);
+}
+
+// NOLINTNEXTLINE
+TEST(Base64Decode, DecodesQuickFox)
+{
+    std::string const input{"VGhlIHF1aWNrIGJyb3duIGZveCBqdW1wcyBvdmVyIHRoZSBsYXp5IGRvZw=="};
+    std::vector<std::uint8_t> const expected{0x54, 0x68, 0x65, 0x20, 0x71, 0x75, 0x69, 0x63, 0x6b, 0x20, 0x62,
+                                             0x72, 0x6f, 0x77, 0x6e, 0x20, 0x66, 0x6f, 0x78, 0x20, 0x6a, 0x75,
+                                             0x6d, 0x70, 0x73, 0x20, 0x6f, 0x76, 0x65, 0x72, 0x20, 0x74, 0x68,
+                                             0x65, 0x20, 0x6c, 0x61, 0x7a, 0x79, 0x20, 0x64, 0x6f, 0x67};
+    auto const actual{endstone::detail::base64_decode<std::vector<std::uint8_t>>(input)};
+    ASSERT_EQ(actual, expected);
+}
+
+// NOLINTNEXTLINE
+TEST(Base64RoundTripTests, AllPossibleBytes)
+{
+    std::vector<std::uint8_t> all_possible_bytes;
+    for (std::size_t i = 0; i <= 255; ++i) {
+        all_possible_bytes.push_back(static_cast<std::uint8_t>(i));
+    }
+
+    auto const encode_string =
+        endstone::detail::base64_encode<std::string>(begin(all_possible_bytes), end(all_possible_bytes));
+    auto const decoded_bytes = endstone::detail::base64_decode<std::vector<std::uint8_t>>(encode_string);
+    ASSERT_EQ(all_possible_bytes, decoded_bytes);
+}
+
+// NOLINTNEXTLINE
+TEST(Base64RoundTripTests, ExhaustiveTests)
+{
+    std::vector<std::string> const base64_strings = {"YW55IGNhcm5hbCBwbGVhcw==",
+                                                     "bGVnYWwgcGFzcw==",
+                                                     "dGVzdCBzdHJpbmc=",
+                                                     "bGVnYWwgcHJvdmlkZXI=",
+                                                     "ZW5vdWdoIHRoZSBzYW1lIG9mIHRoZSBwbGFjZQ==",
+                                                     "YW5vdGhlciB0aGUgc3RyYWlnaHQ=",
+                                                     "d2FzIG1lIGFkZHJlc3MgcHJvdmlkZXI=",
+                                                     "YWJvdXQgdGhlIG1hc3RlciBvZiB0aGUgZGFtYWdl",
+                                                     "ZW50aXJlIHRoYXQgYnJvdWdodCBvZiB0aGUgbW9uZXk=",
+                                                     "bGVnYWwgc2VjdXJpdHk=",
+                                                     "YmFzaWMgZ29vZCBvZiB0aGUgcGFkIHN0cmluZw==",
+                                                     "ZGVsZXRlIHN0cmluZyBvZiB0aGUgc3RyYWlnaHQ=",
+                                                     "YnJvdWdodCBvZiB0aGUgcGFkIGZvbGRlciBvZiB0aGUgZGFtYWdl",
+                                                     "aW50ZXJmYWNlIHN0cmluZw==",
+                                                     "Y29uc29sZS1tZS1jb21wYW55",
+                                                     "aW5mb3JtYXRpb24tbWVkaWE=",
+                                                     "c3RhdHVzLXNlY3VyZQ==",
+                                                     "Y3JlYXRlLWNvbXBhbnktc3RyaW5n",
+                                                     "b3JkZXItbGVhZGVy",
+                                                     "Y2F0YWxvZy1wcm9maWxl",
+                                                     "dGVzdC1jb25zdWx0aW5n",
+                                                     "YnJvdWdodC1sZWFkZXI=",
+                                                     "YXNzaWduLW1lY2hhbmlzbQ==",
+                                                     "bGVnYWwtY29udGFpbmVy",
+                                                     "ZW1haWwtY29udGFpbmVy",
+                                                     "aW5zdGFuY2UtY29udGFpbmVy",
+                                                     "dGVzdC1jb21wYW55LWFuZC1wcm9maWxl",
+                                                     "YmFzZTY0LWJhc2U=",
+                                                     "cGFzc3dvcmQ=",
+                                                     "Zm9vYmFy",
+                                                     "Y29vbC1iYXNl",
+                                                     "YmFzZTY0LXNlY3VyZQ==",
+                                                     "aW50ZXJ2YWw=",
+                                                     "dGhlLW1hc3Rlci1vZi10aGUtZGFtYWdl",
+                                                     "c2FtZS1wbGFjZS1vZi10aGUtZGFtYWdl",
+                                                     "aGFzaC1zb21ldGhpbmc="};
+
+    for (auto const &b64_string : base64_strings) {
+        auto const decoded = endstone::detail::base64_decode(b64_string);
+        ASSERT_TRUE(decoded.has_value());
+        auto const encoded_round_trip = endstone::detail::base64_encode(decoded.value());
+        ASSERT_EQ(encoded_round_trip, b64_string);
+    }
+}
+
+// NOLINTNEXTLINE
 TEST(Base64OverloadTests, EncodesString1)
 {
     std::array<std::pair<std::string, std::string>, 11> const test_cases = {
@@ -138,5 +338,108 @@ TEST(Base64OverloadTests, EncodesString1)
     for (auto const &[input, expected] : test_cases) {
         auto const actual = endstone::detail::base64_encode(input);
         ASSERT_EQ(actual, expected);
+    }
+}
+
+// NOLINTNEXTLINE
+TEST(Base64RoundTripTests, TypeMixTests)
+{
+    const std::string strinput{"Hello, World!"};
+    const std::string stroutput{"SGVsbG8sIFdvcmxkIQ=="};
+
+    typedef std::vector<std::uint8_t> u8vec_type;
+    const u8vec_type uvecinput(strinput.begin(), strinput.end());
+    const u8vec_type uvecoutput(stroutput.begin(), stroutput.end());
+
+    typedef std::vector<std::int8_t> s8vec_type;
+    const s8vec_type svecinput(strinput.begin(), strinput.end());
+    const s8vec_type svecoutput(stroutput.begin(), stroutput.end());
+
+    // str -> str
+    {
+        auto tmp1 = endstone::detail::base64_encode<std::string>(strinput.begin(), strinput.end());
+        ASSERT_EQ(tmp1, stroutput);
+        auto tmp2 = endstone::detail::base64_decode<std::string>(stroutput.begin(), stroutput.end());
+        ASSERT_EQ(tmp2, strinput);
+        auto tmp3 = endstone::detail::base64_encode<std::string>(strinput);
+        ASSERT_EQ(tmp3, stroutput);
+        auto tmp4 = endstone::detail::base64_decode<std::string>(stroutput);
+        ASSERT_EQ(tmp4, strinput);
+        auto tmp5 = endstone::detail::base64_encode(strinput);
+        ASSERT_EQ(tmp5, stroutput);
+        auto tmp6 = endstone::detail::base64_decode(stroutput);
+        ASSERT_EQ(tmp6, strinput);
+    }
+
+    // str -> u8
+    {
+        auto tmp1 = endstone::detail::base64_encode<u8vec_type>(strinput.begin(), strinput.end());
+        ASSERT_EQ(tmp1, uvecoutput);
+        auto tmp2 = endstone::detail::base64_decode<u8vec_type>(stroutput.begin(), stroutput.end());
+        ASSERT_EQ(tmp2, uvecinput);
+        auto tmp3 = endstone::detail::base64_encode<u8vec_type>(strinput);
+        ASSERT_EQ(tmp3, uvecoutput);
+        auto tmp4 = endstone::detail::base64_decode<u8vec_type>(stroutput);
+        ASSERT_EQ(tmp4, uvecinput);
+    }
+
+    // str -> s8
+    {
+        auto tmp1 = endstone::detail::base64_encode<s8vec_type>(strinput.begin(), strinput.end());
+        ASSERT_EQ(tmp1, svecoutput);
+        auto tmp2 = endstone::detail::base64_decode<s8vec_type>(stroutput.begin(), stroutput.end());
+        ASSERT_EQ(tmp2, svecinput);
+        auto tmp3 = endstone::detail::base64_encode<s8vec_type>(strinput);
+        ASSERT_EQ(tmp3, svecoutput);
+        auto tmp4 = endstone::detail::base64_decode<s8vec_type>(stroutput);
+        ASSERT_EQ(tmp4, svecinput);
+    }
+
+    // u8 -> str
+    {
+        auto tmp1 = endstone::detail::base64_encode<std::string>(uvecinput.begin(), uvecinput.end());
+        ASSERT_EQ(tmp1, stroutput);
+        auto tmp2 = endstone::detail::base64_decode<std::string>(uvecoutput.begin(), uvecoutput.end());
+        ASSERT_EQ(tmp2, strinput);
+    }
+
+    // u8 -> u8
+    {
+        auto tmp1 = endstone::detail::base64_encode<u8vec_type>(uvecinput.begin(), uvecinput.end());
+        ASSERT_EQ(tmp1, uvecoutput);
+        auto tmp2 = endstone::detail::base64_decode<u8vec_type>(uvecoutput.begin(), uvecoutput.end());
+        ASSERT_EQ(tmp2, uvecinput);
+    }
+
+    // u8 -> s8
+    {
+        auto tmp1 = endstone::detail::base64_encode<s8vec_type>(uvecinput.begin(), uvecinput.end());
+        ASSERT_EQ(tmp1, svecoutput);
+        auto tmp2 = endstone::detail::base64_decode<s8vec_type>(uvecoutput.begin(), uvecoutput.end());
+        ASSERT_EQ(tmp2, svecinput);
+    }
+
+    // s8 -> str
+    {
+        auto tmp1 = endstone::detail::base64_encode<std::string>(svecinput.begin(), svecinput.end());
+        ASSERT_EQ(tmp1, stroutput);
+        auto tmp2 = endstone::detail::base64_decode<std::string>(svecoutput.begin(), svecoutput.end());
+        ASSERT_EQ(tmp2, strinput);
+    }
+
+    // s8 -> u8
+    {
+        auto tmp1 = endstone::detail::base64_encode<u8vec_type>(svecinput.begin(), svecinput.end());
+        ASSERT_EQ(tmp1, uvecoutput);
+        auto tmp2 = endstone::detail::base64_decode<u8vec_type>(svecoutput.begin(), svecoutput.end());
+        ASSERT_EQ(tmp2, uvecinput);
+    }
+
+    // s8 -> s8
+    {
+        auto tmp1 = endstone::detail::base64_encode<s8vec_type>(svecinput.begin(), svecinput.end());
+        ASSERT_EQ(tmp1, svecoutput);
+        auto tmp2 = endstone::detail::base64_decode<s8vec_type>(svecoutput.begin(), svecoutput.end());
+        ASSERT_EQ(tmp2, svecinput);
     }
 }
