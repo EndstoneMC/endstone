8aa56d140580804b91ee03a473344837e4b818de
Vincent <magicdroidx@gmail.com>
2024-07-16 23:44:43 +0300

    chore: update to game version v1.21.3

--- python/src/endstone/__init__.py
+++ python/src/endstone/__init__.py
@@ -1,7 +1,7 @@
 from endstone._internal.endstone_python import ColorFormat, GameMode, Logger, Player, Server, Skin, Translatable
 from endstone._internal.version import __version__
 
-__minecraft_version__ = "1.21.2"
+__minecraft_version__ = "1.21.3"
 
 __all__ = [
     "__version__",
