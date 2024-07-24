e6dfd43b4517b1c1b8f09c3d86d0bc7700b98d4a
Vincent <magicdroidx@gmail.com>
2024-07-24 17:11:29 +0100

    feat: set executable path for embedded interpreter

--- src/endstone_runtime/main.cpp
+++ src/endstone_runtime/main.cpp
@@ -48,6 +48,14 @@ ENDSTONE_RUNTIME_CTOR int main()
         py::initialize_interpreter(&config);
         py::module_::import("threading");  // https://github.com/pybind/pybind11/issues/2197
         py::module_::import("numpy");      // https://github.com/numpy/numpy/issues/24833
+        py::exec(R"(
+        import os
+        import sys
+        if sys.platform == "win32":
+            sys.executable = os.path.join(sys.prefix, "python.exe")
+        else:
+            sys.executable = os.path.join(sys.prefix, "bin", "python")
+        )");
         py::gil_scoped_release release{};
         release.disarm();
 
