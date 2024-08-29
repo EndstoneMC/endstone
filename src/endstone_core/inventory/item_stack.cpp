b91932fb112a014e7e341e6e7da5a4f8dc7667b1
Vincent <magicdroidx@gmail.com>
2024-08-29 12:30:11 +0100

    feat(item): add EndstoneItemStack

---
+++ src/endstone_core/inventory/item_stack.cpp
@@ -0,0 +1,60 @@
+// Copyright (c) 2024, The Endstone Project. (https://endstone.dev) All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "endstone/detail/inventory/item_stack.h"
+
+namespace endstone::detail {
+
+std::string EndstoneItemStack::getType() const
+{
+    return handle_ != nullptr ? handle_->getItem()->getFullItemName() : "minecraft:air";
+}
+
+void EndstoneItemStack::setType(std::string type)
+{
+    if (getType() == type) {
+        return;
+    }
+    if (type == "minecraft:air") {
+        reset();
+        return;
+    }
+    owned_handle_ = ::ItemStack::create(type, 1);
+    handle_ = owned_handle_.get();
+}
+
+int EndstoneItemStack::getAmount() const
+{
+    return handle_ != nullptr ? handle_->getCount() : 0;
+}
+
+void EndstoneItemStack::setAmount(int amount)
+{
+    const std::uint8_t count = amount & 0xff;
+    if (count == 0) {
+        reset();
+    }
+    if (handle_ == nullptr) {
+        return;
+    }
+    handle_->set(count);
+}
+
+void EndstoneItemStack::reset()
+{
+    handle_ = nullptr;
+    owned_handle_.reset();
+}
+
+}  // namespace endstone::detail
