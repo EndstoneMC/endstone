ff15c53555abde8709669079ea87187255db6f79
Vincent <magicdroidx@gmail.com>
2024-09-10 12:59:54 +0100

    feat: add support for BDS v1.21.23 on Windows

--- python/src/endstone/__init__.py
+++ python/src/endstone/__init__.py
@@ -1,7 +1,7 @@
 from endstone._internal.endstone_python import ColorFormat, GameMode, Logger, Player, Server, Skin, Translatable
 from endstone._internal.version import __version__
 
-__minecraft_version__ = "1.21.22"
+__minecraft_version__ = "1.21.23"
 
 __all__ = [
     "__version__",
