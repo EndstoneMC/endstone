e9491ff861b1b087a4662d3963c5ddd8f0dc45cc
Vincent <magicdroidx@gmail.com>
2024-12-04 18:56:44 +0000

    chore: update symbols.toml for BDS v1.21.50 Windows (not supported yet)

--- python/src/endstone/__init__.py
+++ python/src/endstone/__init__.py
@@ -1,6 +1,6 @@
 from endstone._internal.endstone_python import ColorFormat, GameMode, Logger, Player, Server, Skin
 from endstone._internal.version import __version__
 
-__minecraft_version__ = "1.21.44"
+__minecraft_version__ = "1.21.50"
 
 __all__ = ["__version__", "__minecraft_version__", "ColorFormat", "GameMode", "Logger", "Player", "Server", "Skin"]
