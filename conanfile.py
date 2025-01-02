097cf19d77c388903a36a442b2ba45b48811ce17
Vincent <magicdroidx@gmail.com>
2025-01-02 23:24:44 +0000

    feat: raise errors when attempting to build with unsupported architectures

--- conanfile.py
+++ conanfile.py
@@ -98,6 +98,10 @@ class EndstoneRecipe(ConanFile):
         compiler = self.settings.compiler
         compiler_version = self.settings.compiler.version
 
+        if self.settings.arch != "x86_64":
+            raise ConanInvalidConfiguration(
+                f"{self.ref} can only be built on x86_64. {self.settings.arch} is not supported.")
+
         if self.settings.os == "Windows":
             if not compiler == "msvc" or Version(compiler_version) < self._min_msvc_compiler_version:
                 raise ConanInvalidConfiguration(
