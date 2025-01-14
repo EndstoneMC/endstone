6d40025233a2e23747fc1dfa0658ce7a38669fc9
Vincent <magicdroidx@gmail.com>
2025-01-14 20:27:46 +0000

    chore(deps): update multiple dependencies to their latest versions

--- conanfile.py
+++ conanfile.py
@@ -120,20 +120,20 @@ class EndstoneRecipe(ConanFile):
 
     def requirements(self):
         self.requires("base64/0.5.2")
-        self.requires("boost/1.85.0")
+        self.requires("boost/1.86.0")
         self.requires("concurrentqueue/1.0.4")
-        self.requires("cpptrace/0.7.1")
+        self.requires("cpptrace/0.7.5")
         self.requires("date/3.0.3")
         self.requires("entt/3.14.0")
         self.requires("expected-lite/0.8.0")
         self.requires("fmt/[~10]", transitive_headers=True, transitive_libs=True)
         # self.requires("funchook/1.1.3")
         self.requires("glm/1.0.1")
-        self.requires("magic_enum/0.9.5")
-        self.requires("ms-gsl/4.0.0")
+        self.requires("magic_enum/0.9.7")
+        self.requires("ms-gsl/4.1.0")
         self.requires("nlohmann_json/3.11.3")
-        self.requires("pybind11/2.13.1")
-        self.requires("sentry-native/0.7.15")
+        self.requires("pybind11/2.13.6")
+        self.requires("sentry-native/0.7.17")
         self.requires("spdlog/1.14.1")
         self.requires("tomlplusplus/3.3.0")
 
@@ -143,10 +143,10 @@ class EndstoneRecipe(ConanFile):
         if self._should_enable_devtools:
             self.requires("glew/2.2.0")
             self.requires("glfw/3.4")
-            self.requires("imgui/1.90.8-docking")
+            self.requires("imgui/1.91.5-docking")
             self.requires("zstr/1.0.7")
 
-        self.test_requires("gtest/1.14.0")
+        self.test_requires("gtest/1.15.0")
 
     def config_options(self):
         if self.settings.os == "Windows":
