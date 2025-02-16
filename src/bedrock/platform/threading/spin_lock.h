37edd85996c7a51153ed68e879486c560ece8fcc
Vincent <magicdroidx@gmail.com>
2025-02-16 13:47:04 +0000

    feat: add implementation for `SpinLockImpl`

--- src/bedrock/platform/threading/spin_lock.h
+++ src/bedrock/platform/threading/spin_lock.h
@@ -15,13 +15,80 @@
 #pragma once
 
 #include <atomic>
+#include <system_error>
 #include <thread>
 
-class SpinLock {
+class SpinLockImpl {
+    static const uint32_t LOOP_LIMIT_BEFORE_YIELD = 3000;
+
 public:
+    SpinLockImpl() : no_thread_id_(thread_hasher_(std::thread::id())), owner_thread_(no_thread_id_) {}
+
+    ~SpinLockImpl() = default;
+
+    bool try_lock()
+    {
+        std::size_t current_thread_id = thread_hasher_(std::this_thread::get_id());
+
+        if (owner_thread_ == no_thread_id_) {
+            owner_thread_ = current_thread_id;
+            owner_ref_count_ = 1;
+            return true;
+        }
+
+        if (owner_thread_ != current_thread_id || owner_ref_count_ == -2) {
+            return false;
+        }
+
+        ++owner_ref_count_;
+        return true;
+    }
+
+    void lock()
+    {
+        std::size_t current_thread_id = thread_hasher_(std::this_thread::get_id());
+        int i = LOOP_LIMIT_BEFORE_YIELD;
+        while (true) {
+            if (owner_thread_ == no_thread_id_) {
+                owner_thread_ = current_thread_id;
+                break;
+            }
+
+            if (owner_thread_ == current_thread_id && owner_ref_count_ != -2) {
+                ++owner_ref_count_;
+                return;
+            }
+
+            if (i > 0) {
+                --i;
+            }
+            else {
+                std::this_thread::yield();
+            }
+        }
+        owner_ref_count_ = 1;
+    }
+
+    void unlock()
+    {
+        std::size_t current_thread_id = thread_hasher_(std::this_thread::get_id());
+
+        if (owner_thread_ != current_thread_id || owner_ref_count_ == 0) {
+            throw std::system_error(std::make_error_code(std::errc::operation_not_permitted));
+        }
+
+        if (owner_ref_count_ == 1) {
+            owner_ref_count_ = 0;
+            owner_thread_ = no_thread_id_;
+        }
+        else {
+            --owner_ref_count_;
+        }
+    }
+
 private:
-    std::hash<std::thread::id> thread_hasher_;  // +0
-    std::size_t no_thread_id_;                  // +8
-    std::atomic<std::size_t> owner_thread_;     // +16
-    std::uint32_t owner_ref_count_;             // +24
+    std::hash<std::thread::id> thread_hasher_{};  // +0
+    const std::size_t no_thread_id_;              // +8
+    std::atomic<std::size_t> owner_thread_;       // +16
+    std::uint32_t owner_ref_count_{0};            // +24
 };
