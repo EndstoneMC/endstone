f639c27b288a086db972f6bba4a96b13ff30ab01
Vincent <magicdroidx@gmail.com>
2025-03-18 18:46:42 +0000

    refactor: process packet only once before iterating over recipients

--- src/endstone/core/event/handlers/server_network_event_handler.cpp
+++ src/endstone/core/event/handlers/server_network_event_handler.cpp
@@ -92,18 +92,18 @@ bool EndstoneServerNetworkEventHandler::handleEvent(IncomingPacketEvent &event)
 bool EndstoneServerNetworkEventHandler::handleEvent(OutgoingPacketEvent &event)
 {
     const auto &server = entt::locator<EndstoneServer>::value();
+    const auto &network = server.getServer().getNetwork();
+    ReadOnlyBinaryStream stream(network.send_stream_.getView(), false);
+    auto header = stream.getUnsignedVarInt().logError(Bedrock::LogLevel::Error, LogAreaID::Network);
+    if (!header.has_value()) {
+        return false;
+    }
+    const auto packet_id = static_cast<int>(header.value() & 0x3ff);
+    const auto sender_sub_id = (header.value() >> 10) & 3;
+    const auto target_sub_id = (header.value() >> 12) & 3;
+
     for (auto it = event.recipients.begin(); it != event.recipients.end();) {
         const auto &recipient = *it;
-        const auto &network = server.getServer().getNetwork();
-        ReadOnlyBinaryStream stream(network.send_stream_.getView(), false);
-        auto header = stream.getUnsignedVarInt().logError(Bedrock::LogLevel::Error, LogAreaID::Network);
-        if (!header.has_value()) {
-            return false;
-        }
-        const auto packet_id = static_cast<int>(header.value() & 0x3ff);
-        const auto sender_sub_id = (header.value() >> 10) & 3;
-        const auto target_sub_id = (header.value() >> 12) & 3;
-
         if (const auto *player = WeakEntityRef(recipient).tryUnwrap<::Player>(); player) {
             PacketSendEvent e{player->getEndstoneActor<EndstonePlayer>(), packet_id,
                               stream.getView().substr(stream.getReadPointer())};
