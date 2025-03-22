84f56c9d1a5eb2cc41f2679e2251f1f05556380b
Vincent <magicdroidx@gmail.com>
2025-03-22 00:18:43 +0000

    chore: update Minecraft version to 1.21.80-preview.21

--- endstone/__init__.py
+++ endstone/__init__.py
@@ -1,7 +1,7 @@
 from endstone._internal.endstone_python import ColorFormat, GameMode, Logger, OfflinePlayer, Player, Server, Skin
 from endstone._internal.version import __version__
 
-__minecraft_version__ = "1.21.70-preview.26"
+__minecraft_version__ = "1.21.80-preview.21"
 
 __all__ = [
     "__version__",
