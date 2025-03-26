7e44dec2d7242d30789d713f84ff25dea80d70c5
Vincent <magicdroidx@gmail.com>
2025-03-26 22:58:12 +0000

    feat: add `NetworkConnection` class

---
+++ src/bedrock/network/network_connection.h
@@ -0,0 +1,57 @@
+// Copyright (c) 2024, The Endstone Project. (https://endstone.dev) All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <queue>
+
+class NetworkConnection {
+public:
+    enum class Type : int {
+        Remote = 0,
+        Local = 1,
+    };
+    NetworkConnection() = delete;
+
+    [[nodiscard]] bool shouldCloseConnection() const
+    {
+        return should_close_connection_;
+    }
+
+    NetworkIdentifier id;
+    Type type;
+    std::weak_ptr<class NetworkPacketRecorder> network_packet_recorder;
+    std::weak_ptr<class EncryptedNetworkPeer> encrypted_peer;
+    std::weak_ptr<class CompressedNetworkPeer> compressed_peer;
+    std::weak_ptr<class BatchedNetworkPeer> batched_peer;
+    std::weak_ptr<class LatencyNetworkPeer> latency_peer;
+    std::shared_ptr<NetworkPeer> peer;
+    // std::shared_ptr<NetworkPeer> transport_peer;
+    std::chrono::steady_clock::time_point last_packet_time;
+    std::chrono::steady_clock::time_point closed_time;
+
+protected:
+    struct PausedPacket {
+        std::string data;
+        NetworkPeer::PacketRecvTimepoint timepoint;
+    };
+    std::bitset<2UL> paused_channels_;
+    std::queue<PausedPacket> resumed_packets_;
+    std::array<std::vector<PausedPacket>, 2UL> paused_packets_;
+
+private:
+    bool disconnected_;
+    bool should_close_connection_;  // +377
+    bool encryption_disabled_;
+};
