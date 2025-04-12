a75067827ba6b3cebc213aad01093a54c0b0bd83
Vincent <magicdroidx@gmail.com>
2025-04-12 16:54:56 +0800

    chore: update BDS to 1.21.73.01

--- endstone/__init__.py
+++ endstone/__init__.py
@@ -1,7 +1,7 @@
 from endstone._internal.endstone_python import ColorFormat, GameMode, Logger, OfflinePlayer, Player, Server, Skin
 from endstone._internal.version import __version__
 
-__minecraft_version__ = "1.21.72"
+__minecraft_version__ = "1.21.73"
 
 __all__ = [
     "__version__",
