463bf5e2a240751cfeecab91ee1856f376ccd113
Vincent <magicdroidx@gmail.com>
2025-05-08 14:29:39 +0100

    refactor: move implementations to `EndstoneItemMeta`

---
+++ src/endstone/core/inventory/meta/item_meta.cpp
@@ -0,0 +1,265 @@
+// Copyright (c) 2024, The Endstone Project. (https://endstone.dev) All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "endstone/core/inventory/meta/item_meta.h"
+
+#include <utility>
+
+#include <bedrock/world/item/enchanting/enchant.h>
+#include <fmt/chrono.h>
+#include <nlohmann/detail/value_t.hpp>
+
+#include "bedrock/world/item/item.h"
+#include "bedrock/world/item/item_stack_base.h"
+
+namespace endstone::core {
+
+namespace {
+std::unique_ptr<ListTag> createStringList(const std::vector<std::string> &list)
+{
+    auto list_tag = std::make_unique<ListTag>();
+    for (const auto &value : list) {
+        list_tag->add(std::make_unique<StringTag>(value));
+    }
+    return list_tag;
+}
+
+void setDisplayTag(::CompoundTag &tag, std::string key, std::unique_ptr<Tag> value)
+{
+    auto *display = tag.getCompound(ItemStackBase::TAG_DISPLAY);
+    if (!display) {
+        display = tag.putCompound(ItemStackBase::TAG_DISPLAY, std::make_unique<CompoundTag>());
+    }
+
+    display->put(std::move(key), std::move(value));
+}
+
+std::unordered_map<std::string, int> buildEnchantments(const ::CompoundTag &tag)
+{
+    if (!tag.contains(ItemStackBase::TAG_ENCHANTS)) {
+        return {};
+    }
+
+    const auto *ench = tag.getList(ItemStackBase::TAG_ENCHANTS);
+    if (!ench) {
+        return {};
+    }
+
+    std::unordered_map<std::string, int> enchantments;
+    ench->forEachCompoundTag([&](const CompoundTag &enchant_tag) {
+        auto id = enchant_tag.getShort("id");
+        auto lvl = enchant_tag.getShort("lvl") & 0xffff;
+        const auto *enchant = Enchant::getEnchant(static_cast<Enchant::Type>(id));
+        if (enchant) {
+            enchantments[enchant->getStringId().getString()] = lvl;
+        }
+    });
+
+    return enchantments;
+}
+
+void applyEnchantments(const std::unordered_map<std::string, int> &enchantments, ::CompoundTag &tag)
+{
+    if (enchantments.empty()) {
+        return;
+    }
+
+    auto list = std::make_unique<ListTag>();
+
+    for (const auto &[key, lvl] : enchantments) {
+        auto subtag = std::make_unique<CompoundTag>();
+
+        const auto *enchant = Enchant::getEnchantFromName(key);
+        if (!enchant) {
+            continue;
+        }
+
+        subtag->putShort("id", static_cast<std::int16_t>(enchant->getEnchantType()));
+        subtag->putShort("lvl", static_cast<std::int16_t>(lvl));
+
+        list->add(std::move(subtag));
+    }
+
+    tag.put(ItemStackBase::TAG_ENCHANTS, std::move(list));
+}
+}  // namespace
+
+EndstoneItemMeta::EndstoneItemMeta(const ::CompoundTag &tag)
+{
+    if (const auto *display = tag.getCompound(ItemStackBase::TAG_DISPLAY)) {
+        // Display name
+        auto display_name = display->getString(ItemStackBase::TAG_DISPLAY_NAME);
+        if (!display_name.empty()) {
+            display_name_ = display_name;
+        }
+
+        // Lore
+        if (const auto *lore_tag = display->getList(ItemStackBase::TAG_LORE)) {
+            std::vector<std::string> lore;
+            lore.reserve(lore_tag->size());
+            for (auto i = 0; i < lore_tag->size(); i++) {
+                lore.emplace_back(lore_tag->getString(i));
+            }
+            if (!lore.empty()) {
+                lore_ = std::move(lore);
+            }
+        }
+    }
+
+    // Enchant
+    enchantments_ = buildEnchantments(tag);
+
+    // Damage
+    if (const auto damage = tag.getInt(Item::TAG_DAMAGE)) {
+        damage_ = damage;
+    }
+}
+
+ItemMeta::Type EndstoneItemMeta::getType() const
+{
+    return Type::Item;
+}
+
+bool EndstoneItemMeta::isEmpty() const
+{
+    return !(hasDisplayName() || hasLore() || hasEnchants() || hasDamage());
+}
+
+std::unique_ptr<ItemMeta> EndstoneItemMeta::clone() const
+{
+    return std::make_unique<EndstoneItemMeta>(*this);
+}
+
+bool EndstoneItemMeta::hasDisplayName() const
+{
+    return display_name_.has_value();
+}
+
+std::optional<std::string> EndstoneItemMeta::getDisplayName() const
+{
+    if (!hasDisplayName()) {
+        return std::nullopt;
+    }
+    return display_name_;
+}
+
+void EndstoneItemMeta::setDisplayName(std::optional<std::string> name)
+{
+    if (!name.has_value() || name.value().empty()) {
+        display_name_.reset();
+    }
+    else {
+        display_name_ = std::move(name.value());
+    }
+}
+
+bool EndstoneItemMeta::hasLore() const
+{
+    return !lore_.empty();
+}
+
+std::optional<std::vector<std::string>> EndstoneItemMeta::getLore() const
+{
+    if (!hasLore()) {
+        return std::nullopt;
+    }
+    return lore_;
+}
+
+void EndstoneItemMeta::setLore(std::optional<std::vector<std::string>> lore)
+{
+    if (!lore.has_value() || lore.value().empty()) {
+        lore_.clear();
+    }
+    else {
+        lore_ = std::move(lore.value());
+    }
+}
+
+bool EndstoneItemMeta::hasDamage() const
+{
+    return damage_ > 0;
+}
+
+int EndstoneItemMeta::getDamage() const
+{
+    return damage_;
+}
+
+void EndstoneItemMeta::setDamage(int damage)
+{
+    damage_ = damage;
+}
+
+bool EndstoneItemMeta::hasEnchants() const
+{
+    return !enchantments_.empty();
+}
+
+bool EndstoneItemMeta::hasEnchant(const std::string &id) const
+{
+    return hasEnchants() && enchantments_.contains(id);
+}
+
+int EndstoneItemMeta::getEnchantLevel(const std::string &id) const
+{
+    if (!hasEnchant(id)) {
+        return 0;
+    }
+    return enchantments_.at(id);
+}
+
+std::unordered_map<std::string, int> EndstoneItemMeta::getEnchants() const
+{
+    if (hasEnchants()) {
+        return enchantments_;
+    }
+    return {};
+}
+
+bool EndstoneItemMeta::addEnchant(const std::string &id, int level, bool force)
+{
+    // TODO: we should do the level limit check here
+    const auto old = getEnchantLevel(id);
+    enchantments_[id] = level;
+    return old == 0 || old != level;
+}
+
+bool EndstoneItemMeta::removeEnchant(const std::string &id)
+{
+    return enchantments_.erase(id) > 0;
+}
+
+void EndstoneItemMeta::removeEnchants()
+{
+    enchantments_.clear();
+}
+
+void EndstoneItemMeta::applyToItem(CompoundTag &tag) const
+{
+    if (hasDisplayName()) {
+        setDisplayTag(tag, ItemStackBase::TAG_DISPLAY_NAME, std::make_unique<StringTag>(display_name_.value()));
+    }
+
+    if (hasLore()) {
+        setDisplayTag(tag, ItemStackBase::TAG_LORE, createStringList(lore_));
+    }
+
+    applyEnchantments(enchantments_, tag);
+
+    if (hasDamage()) {
+        tag.putInt(Item::TAG_DAMAGE, damage_);
+    }
+}
+}  // namespace endstone::core
---
+++ src/endstone/core/inventory/meta/item_meta.h
@@ -0,0 +1,53 @@
+// Copyright (c) 2024, The Endstone Project. (https://endstone.dev) All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include "bedrock/nbt/compound_tag.h"
+#include "endstone/inventory/meta/item_meta.h"
+
+namespace endstone::core {
+
+class EndstoneItemMeta : public ItemMeta {
+public:
+    EndstoneItemMeta(const ::CompoundTag &tag);
+    [[nodiscard]] Type getType() const override;
+    [[nodiscard]] bool isEmpty() const override;
+    [[nodiscard]] std::unique_ptr<ItemMeta> clone() const override;
+    [[nodiscard]] bool hasDisplayName() const override;
+    [[nodiscard]] std::optional<std::string> getDisplayName() const override;
+    void setDisplayName(std::optional<std::string> name) override;
+    [[nodiscard]] bool hasLore() const override;
+    [[nodiscard]] std::optional<std::vector<std::string>> getLore() const override;
+    void setLore(std::optional<std::vector<std::string>> lore) override;
+    [[nodiscard]] bool hasDamage() const override;
+    [[nodiscard]] int getDamage() const override;
+    void setDamage(int damage) override;
+    [[nodiscard]] bool hasEnchants() const override;
+    [[nodiscard]] bool hasEnchant(const std::string &id) const override;
+    [[nodiscard]] int getEnchantLevel(const std::string &id) const override;
+    [[nodiscard]] std::unordered_map<std::string, int> getEnchants() const override;
+    bool addEnchant(const std::string &id, int level, bool force) override;
+    bool removeEnchant(const std::string &id) override;
+    void removeEnchants() override;
+    virtual void applyToItem(::CompoundTag &tag) const;
+
+private:
+    std::optional<std::string> display_name_;
+    std::vector<std::string> lore_;
+    std::unordered_map<std::string, int> enchantments_;
+    int damage_ = 0;
+};
+
+}  // namespace endstone::core
