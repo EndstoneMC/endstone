d8acbb9489e1767e34245eaa592def6f6ee4749e
Vincent <magicdroidx@gmail.com>
2025-05-14 20:04:48 +0100

    style: code formatting

--- scripts/generate_stubs.py
+++ scripts/generate_stubs.py
@@ -6,9 +6,10 @@ def main():
     output_path = Path(__file__).resolve().parent.parent
     command = [
         "pybind11-stubgen",
-        "-o", str(output_path),
+        "-o",
+        str(output_path),
         "--enum-class-locations=.+:endstone._internal.endstone_python",
-        "endstone._internal.endstone_python"
+        "endstone._internal.endstone_python",
     ]
     subprocess.run(command, check=True)
     print(f"Stubs generated successfully to: {output_path}")
