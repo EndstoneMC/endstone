639826b8c77cd607451812821752e9d2d56a9a41
Vincent <magicdroidx@gmail.com>
2025-05-21 21:36:15 +0100

    chore: update BDS to 1.21.83
    (cherry picked from commit 59fec7d0b3ae069c74de8079b7372d0090c3c437)

--- endstone/__init__.py
+++ endstone/__init__.py
@@ -12,7 +12,7 @@ from endstone._internal.endstone_python import (
 
 from endstone._internal.version import __version__
 
-__minecraft_version__ = "1.21.82"
+__minecraft_version__ = "1.21.83"
 
 __all__ = [
     "__version__",
