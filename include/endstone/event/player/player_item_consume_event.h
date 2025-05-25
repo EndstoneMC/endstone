dde8b100563a6749ee6562cfcf3cc7e2154c41d9
Vincent <magicdroidx@gmail.com>
2025-05-25 16:19:43 +0100

    refactor: update methods in `PlayerItemConsumeEvent`

--- include/endstone/event/player/player_item_consume_event.h
+++ include/endstone/event/player/player_item_consume_event.h
@@ -16,25 +16,58 @@
 
 #include "endstone/event/cancellable.h"
 #include "endstone/event/player/player_event.h"
+#include "endstone/inventory/equipment_slot.h"
 #include "endstone/inventory/item_stack.h"
 
 namespace endstone {
-class PlayerItemConsumeEvent : public Cancellable<PlayerEvent> {
+class PlayerItemConsumeEvent final : public Cancellable<PlayerEvent> {
 public:
     ENDSTONE_EVENT(PlayerItemConsumeEvent)
 
-    explicit PlayerItemConsumeEvent(Player &player, ItemStack &item) : Cancellable(player), item_(item) {}
-    /**
-     * @brief Returns the item in hand represented by this event
-     *
-     * @return ItemStack the item used
-     */
-    [[nodiscard]] ItemStack &getItem() const
+    explicit PlayerItemConsumeEvent(Player &player, const ItemStack &item, EquipmentSlot hand)
+        : Cancellable(player), item_(item.clone()), hand_(hand)
     {
-        return item_;
+    }
+
+    /**
+     * @brief Gets the item that is being consumed.
+     *
+     * @note Modifying the returned item will have no effect, you must use setItem(ItemStack) instead.
+     *
+     * @return an ItemStack for the item being consumed
+     */
+    [[nodiscard]] std::unique_ptr<ItemStack> getItem() const
+    {
+        return item_->clone();
+    }
+
+    /**
+     * @brief Set the item being consumed
+     *
+     * @param item the item being consumed
+     */
+    void setItem(std::unique_ptr<ItemStack> item)
+    {
+        if (item == nullptr) {
+            item_ = std::make_unique<ItemStack>("minecraft:air");
+        }
+        else {
+            item_ = std::move(item);
+        }
+    }
+
+    /**
+     * @brief Get the hand used to consume the item.
+     *
+     * @return the hand
+     */
+    [[nodiscard]] EquipmentSlot getHand() const
+    {
+        return hand_;
     }
 
 private:
-    ItemStack &item_;
+    std::unique_ptr<ItemStack> item_;
+    EquipmentSlot hand_;
 };
 }  // namespace endstone
