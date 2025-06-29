ea5e0bfbc7274ea5a9f0c3d0e6fbcfb759bc7ee7
killcerr <kill_cerr@outlook.com>
2025-06-29 07:26:53 -0700

    feat: add Leaves Decay event (#209)
    * feat: add Leaves Decay event
    
    * feat: add Leaves Decay event
    
    * feat: add Leaves Decay event
    
    * feat: add Leaves Decay event
    
    * feat: update implementation
    
    * feat: add import for LeavesDecayEvent in event module
    
    ---------
    
    Co-authored-by: killcerr <killcerr@outlook.com>
    Co-authored-by: Vincent <magicdroidx@gmail.com>

---
+++ src/endstone/runtime/bedrock_hooks/leaves_block.cpp
@@ -0,0 +1,19 @@
+#include "bedrock/world/level/block/leaves_block.h"
+
+#include "bedrock/world/level/block_source.h"
+#include "endstone/core/block/block.h"
+#include "endstone/core/server.h"
+#include "endstone/event/block/leaves_decay_event.h"
+#include "endstone/runtime/hook.h"
+
+void LeavesBlock::_die(::BlockSource &region, ::BlockPos const &pos) const
+{
+    const auto &server = endstone::core::EndstoneServer::getInstance();
+    const auto block = endstone::core::EndstoneBlock::at(region, pos);
+    endstone::LeavesDecayEvent event(*block);
+    server.getPluginManager().callEvent(event);
+    if (event.isCancelled()) {
+        return;
+    }
+    ENDSTONE_HOOK_CALL_ORIGINAL(&LeavesBlock::_die, this, region, pos);
+}
