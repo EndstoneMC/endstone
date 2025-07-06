69190b8a8e09fca616489526fb701fd2b6460e53
Vincent <magicdroidx@gmail.com>
2025-07-06 16:34:15 +0100

    fix: correct the implementation of `Recipes::getFurnaceRecipeResult`

--- src/bedrock/world/item/crafting/recipes.cpp
+++ src/bedrock/world/item/crafting/recipes.cpp
@@ -1,24 +1,33 @@
+// Copyright (c) 2024, The Endstone Project. (https://endstone.dev) All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include "bedrock/world/item/crafting/recipes.h"
-[[nodiscard]] ItemInstance Recipes::getFurnaceRecipeResult(::ItemStackBase const &item, ::HashedString const &tag) const
+
+ItemInstance Recipes::getFurnaceRecipeResult(const ItemStackBase &item, const HashedString &tag) const
 {
-    if (item) {
-        if (FurnaceRecipeKey key = {.id = item.getId() << 16 | item.getAuxValue(), .tag = tag};
-            furnace_recipes_.contains(key)) {
-            return furnace_recipes_.at(key);
+    if (!item || furnace_recipes_.empty()) {
+        return ItemInstance();
+    }
+
+    for (const auto &[key, value] : furnace_recipes_) {
+        auto id = key.id >> 16;
+        auto aux = static_cast<std::uint16_t>(key.id);
+        if (id == item.getId() && key.tag == tag &&
+            (aux == ItemDescriptor::ANY_AUX_VALUE || aux == item.getAuxValue())) {
+            return value;
         }
-        if (FurnaceRecipeKey key = {.id = item.getId() << 16 | 0x7fff, .tag = tag}; furnace_recipes_.contains(key)) {
-            return furnace_recipes_.at(key);
-        }
     }
-    return {};
-}
-bool Recipes::FurnaceRecipeKey::operator<(const FurnaceRecipeKey &other) const
-{
-    if (id != other.id) {
-        return id < other.id;
-    }
-    if (tag.getHash() != other.tag.getHash()) {
-        return tag.getHash() < other.tag.getHash();
-    }
-    return tag.getString() < other.tag.getString();
+
+    return ItemInstance();
 }
--- src/bedrock/world/item/crafting/recipes.h
+++ src/bedrock/world/item/crafting/recipes.h
@@ -14,37 +14,50 @@
 
 #pragma once
 
+#include <functional>
+#include <memory>
+
 #include "bedrock/core/string/string_hash.h"
 #include "bedrock/resources/resource_pack_manager.h"
 #include "bedrock/world/item/crafting/recipe.h"
 
+using RecipeListenerList = std::vector<std::pair<std::weak_ptr<bool>, std::function<void()>>>;
+
 class ExternalRecipeStore {
-    char filler_[8];
+private:
+    BlockReducer *block_reducer_ = nullptr;
 };
 
-struct SortItemInstanceIdAux {};
-
 class Level;
-
 class Recipes {
+public:
+    static constexpr int RECIPE_MAXIMUM_WIDTH = 3;
+    static constexpr int RECIPE_MAXIMUM_HEIGHT = 3;
+    static constexpr int RECIPE_TOP_PRIORITY = 0;
+    static constexpr int DEFAULT_PRIORITY = 50;
+
     struct FurnaceRecipeKey {
+        FurnaceRecipeKey(int id, const HashedString &tag) : id(id), tag(tag) {}
+        bool operator<(const FurnaceRecipeKey &) const;
         int id;
         HashedString tag;
-        bool operator<(const FurnaceRecipeKey &) const;
     };
+
+    Recipes();
+    Recipes(Level *);
+    [[nodiscard]] ItemInstance getFurnaceRecipeResult(const ItemStackBase &, const HashedString &) const;
+
+private:
     ResourcePackManager *resource_pack_manager_;
     ExternalRecipeStore external_recipe_store_;
     std::map<HashedString, std::map<std::string, std::shared_ptr<Recipe>>> recipes_;
     std::map<FurnaceRecipeKey, ItemInstance> furnace_recipes_;
     bool initializing_;
     std::map<ItemInstance, std::unordered_map<std::string, Recipe *>, SortItemInstanceIdAux> recipes_by_output_;
-    std::unordered_map<RecipeNetId, Recipe *> recipes_by_net_id_;
-    std::unordered_map<uint64_t, std::unordered_map<uint64_t, std::shared_ptr<std::vector<ItemInstance>>>>
+    std::unordered_map<TypedServerNetId<RecipeNetIdTag, std::uint32_t, 0U>, Recipe *> recipes_by_net_id_;
+    std::unordered_map<std::uint64_t, std::unordered_map<std::uint64_t, std::shared_ptr<std::vector<ItemInstance>>>>
         recipes_by_input_;
     std::vector<gsl::not_null<Recipe *>> unlockable_recipes_;
-    std::vector<std::pair<std::weak_ptr<bool>, ::std::function<void()>>> listeners_;
+    RecipeListenerList listeners_;
     Level *level_;
-
-public:
-    [[nodiscard]] ItemInstance getFurnaceRecipeResult(::ItemStackBase const &item, ::HashedString const &tag) const;
 };
