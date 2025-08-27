47d51074bfbfa0ed48b0910d26608fcbfd1d5f15
Vincent <magicdroidx@gmail.com>
2025-08-27 14:45:52 +0100

    feat: add `setInactivityTimeout` to `RemoteConnector` interface

--- src/bedrock/network/remote_connector.h
+++ src/bedrock/network/remote_connector.h
@@ -31,6 +31,6 @@ public:
     [[nodiscard]] virtual NetworkIdentifier getNetworkIdentifier() const = 0;
     virtual bool setApplicationHandshakeCompleted(const NetworkIdentifier &) = 0;
     virtual void setDisableLanSignaling(bool) = 0;
-    virtual void unknown() = 0; // TODO(fixme): what is this?
+    virtual void setInactivityTimeout(NetworkIdentifier const &, std::optional<std::chrono::seconds>) = 0;
 };
 static_assert(sizeof(RemoteConnector) == 80);
