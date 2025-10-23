750874759a728f7635da00bd97463f2139c0649b
Vincent <magicdroidx@gmail.com>
2025-10-23 23:26:09 +0100

    feat: add support for BDS v1.21.114

--- endstone/__init__.py
+++ endstone/__init__.py
@@ -12,7 +12,7 @@ from endstone._internal.endstone_python import (
 )
 from endstone._internal.version import __version__
 
-__minecraft_version__ = "1.21.113"
+__minecraft_version__ = "1.21.114"
 
 __all__ = [
     "__version__",
