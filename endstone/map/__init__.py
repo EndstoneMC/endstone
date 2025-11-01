8d26db4978e6d8deaf105d63c6216a3a14b69a79
Vincent <magicdroidx@gmail.com>
2025-11-01 18:21:38 +0000

    feat: add `MapCursor` to `endstone.map` module exports

--- endstone/map/__init__.py
+++ endstone/map/__init__.py
@@ -3,6 +3,6 @@ import lazy_loader as lazy
 __getattr__, __dir__, __all__ = lazy.attach(
     "endstone._python",
     submod_attrs={
-        "map": ["MapCanvas", "MapRenderer", "MapView"],
+        "map": ["MapCanvas", "MapCursor", "MapRenderer", "MapView"],
     },
 )
