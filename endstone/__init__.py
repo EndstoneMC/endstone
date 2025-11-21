c7a25f0e6accd03a3a6964c7f643d4fcfcc77d0d
Vincent <magicdroidx@gmail.com>
2025-11-21 17:59:31 +0000

    feat: add support for v1.21.124.2

--- endstone/__init__.py
+++ endstone/__init__.py
@@ -12,7 +12,7 @@ from endstone._internal.endstone_python import (
 )
 from endstone._internal.version import __version__
 
-__minecraft_version__ = "1.21.123"
+__minecraft_version__ = "1.21.124"
 
 __all__ = [
     "__version__",
