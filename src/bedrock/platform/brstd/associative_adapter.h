e859deee0b0d1c839c39b8e24310573e0374c409
Vincent <magicdroidx@gmail.com>
2025-12-10 19:55:00 +0000

    feat: add `associative_adapter::find`

--- src/bedrock/platform/brstd/associative_adapter.h
+++ src/bedrock/platform/brstd/associative_adapter.h
@@ -15,24 +15,69 @@
 #pragma once
 
 namespace brstd {
-
 struct no_value_t {};
 
-struct no_mapped_container_t {};
+struct no_mapped_container_t {
+    struct iterator {
+        using difference_type = ptrdiff_t;
+        std::strong_ordering operator<=>(const iterator &) const = default;
+        no_value_t &operator*() const
+        {
+            static no_value_t value;
+            return value;
+        }
+        iterator &operator++() { return *this; }
+        iterator operator++(int) { return *this; }
+        iterator &operator--() { return *this; }
+        iterator operator--(int) { return *this; }
+        iterator operator+(difference_type) const { return *this; }
+        iterator operator-(difference_type) const { return *this; }
+        iterator &operator+=(difference_type) { return *this; }
+        iterator &operator-=(difference_type) { return *this; }
+    };
+    using const_iterator = iterator;
+    std::strong_ordering operator<=>(const no_mapped_container_t &) const = default;
+    iterator begin() const { return iterator(); }
+    iterator end() const { return iterator(); }
+    iterator rbegin() const { return iterator(); }
+    iterator rend() const { return iterator(); }
+    iterator cbegin() const { return iterator(); }
+    iterator cend() const { return iterator(); }
+    iterator crbegin() const { return iterator(); }
+    iterator crend() const { return iterator(); }
+};
 
 template <typename Key, typename T, typename Compare, typename KeyContainer, typename MappedContainer>
 class associative_adapter {
-    template <typename, typename, typename>
+    template <typename KeysIterator, typename ValuesIterator, typename ValueType>
     class iterator_impl;
 
 public:
     static constexpr bool no_mapped_container = std::is_same_v<MappedContainer, no_mapped_container_t>;
+    template <bool NoMappedContainer>
+    struct traits_t;
+    template <>
+    struct traits_t<true> {
+        using mapped_type = Key;
+        using value_type = Key;
+    };
+    template <>
+    struct traits_t<false> {
+        using mapped_type = T;
+        using value_type = std::pair<Key, T>;
+    };
     using key_type = Key;
     using key_compare = Compare;
+    using value_type = traits_t<no_mapped_container>::value_type;
     using size_type = KeyContainer::size_type;
     using difference_type = KeyContainer::difference_type;
     using key_container_type = KeyContainer;
     using mapped_container_type = MappedContainer;
+    using iterator = iterator_impl<typename key_container_type::iterator, typename mapped_container_type::iterator,
+                                   std::conditional_t<no_mapped_container, const Key &, std::pair<const Key &, T &>>>;
+    using const_iterator =
+        iterator_impl<typename key_container_type::const_iterator, typename mapped_container_type::const_iterator,
+                      std::conditional_t<no_mapped_container, const Key &, std::pair<const Key &, const T &>>>;
 
 private:
     template <typename Derived, typename KeysIterator, typename ValuesIterator>
@@ -86,29 +131,34 @@ private:
             return static_cast<Derived &>(*this);
         }
 
-        Derived operator+(difference_type n) const
-        {
-            return Derived(keys_it_ + n, values_it_ + n);
-        }
+        Derived operator+(difference_type n) const { return Derived(keys_it_ + n, values_it_ + n); }
 
-        Derived operator-(difference_type n) const
-        {
-            return Derived(keys_it_ - n, values_it_ - n);
-        }
+        Derived operator-(difference_type n) const { return Derived(keys_it_ - n, values_it_ - n); }
 
-        bool operator==(const iterator_impl_base &other) const
-        {
-            return keys_it_ == other._keys_it;
-        }
+        bool operator==(const iterator_impl_base &other) const { return keys_it_ == other.keys_it_; }
 
-        std::strong_ordering operator<=>(iterator_impl_base &other) const
-        {
-            return keys_it_ <=> other._keys_it;
-        }
+        std::strong_ordering operator<=>(iterator_impl_base &other) const { return keys_it_ <=> other.keys_it_; }
 
     protected:
         KeysIterator keys_it_;
         ValuesIterator values_it_;
+    };
+
+    template <typename KeysIterator, typename ValuesIterator, typename ValueType>
+    class iterator_impl : public iterator_impl_base<iterator_impl<KeysIterator, ValuesIterator, ValueType>,
+                                                    KeysIterator, ValuesIterator> {
+    public:
+        using iterator_impl_base<iterator_impl, KeysIterator, ValuesIterator>::iterator_impl_base;
+        ValueType operator*() const
+        {
+            if constexpr (no_mapped_container) {
+                return *iterator_impl_base<iterator_impl, KeysIterator, ValuesIterator>::keys_it_;
+            }
+            else {
+                return {*iterator_impl_base<iterator_impl, KeysIterator, ValuesIterator>::keys_it_,
+                        *iterator_impl_base<iterator_impl, KeysIterator, ValuesIterator>::values_it_};
+            }
+        }
     };
 
 public:
@@ -117,24 +167,39 @@ public:
         mapped_container_type values;  // +24
     };
 
-    auto begin() noexcept
+    iterator begin() noexcept { return iterator(containers_.keys.begin(), containers_.values.begin()); }
+
+    const_iterator begin() const noexcept
     {
-        return containers_.keys.begin();
+        return const_iterator(containers_.keys.begin(), containers_.values.begin());
     }
 
-    auto begin() const noexcept
+    iterator end() noexcept { return iterator(containers_.keys.end(), containers_.values.end()); }
+
+    const_iterator end() const noexcept { return const_iterator(containers_.keys.end(), containers_.values.end()); }
+
+    iterator find(const key_type &key)
     {
-        return containers_.keys.begin();
+        auto first = containers_.keys.begin();
+        auto last = containers_.keys.end();
+        auto it = std::lower_bound(first, last, key, compare_);
+        if (it != last && !compare_(*it, key) && !compare_(key, *it)) {
+            auto offset = static_cast<difference_type>(it - first);
+            return iterator(it, containers_.values.begin() + offset);
+        }
+        return end();
     }
 
-    auto end() noexcept
+    const_iterator find(const key_type &key) const
     {
-        return containers_.keys.end();
-    }
-
-    auto end() const noexcept
-    {
-        return containers_.keys.end();
+        auto first = containers_.keys.begin();
+        auto last = containers_.keys.end();
+        auto it = std::lower_bound(first, last, key, compare_);
+        if (it != last && !compare_(*it, key) && !compare_(key, *it)) {
+            auto offset = static_cast<difference_type>(it - first);
+            return const_iterator(it, containers_.values.begin() + offset);
+        }
+        return end();
     }
 
 private:
