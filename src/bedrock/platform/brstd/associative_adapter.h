ec9061d0211aae7fccbd24a57d6ee92b96d0593c
Vincent <magicdroidx@gmail.com>
2025-12-10 18:57:43 +0000

    refactor: implement `iterator_impl` and `iterator_impl_base` in `associative_adapter`

--- src/bedrock/platform/brstd/associative_adapter.h
+++ src/bedrock/platform/brstd/associative_adapter.h
@@ -22,15 +22,94 @@ struct no_mapped_container_t {};
 
 template <typename Key, typename T, typename Compare, typename KeyContainer, typename MappedContainer>
 class associative_adapter {
+    template <typename, typename, typename>
+    class iterator_impl;
+
 public:
     static constexpr bool no_mapped_container = std::is_same_v<MappedContainer, no_mapped_container_t>;
-
-private:
     using key_type = Key;
     using key_compare = Compare;
-    using size_type = typename std::vector<key_type>::size_type;
+    using size_type = KeyContainer::size_type;
+    using difference_type = KeyContainer::difference_type;
     using key_container_type = KeyContainer;
     using mapped_container_type = MappedContainer;
+
+private:
+    template <typename Derived, typename KeysIterator, typename ValuesIterator>
+    class iterator_impl_base {
+    public:
+        iterator_impl_base() = default;
+        iterator_impl_base(KeysIterator keys_it, ValuesIterator values_it) : keys_it_(keys_it), values_it_(values_it) {}
+        iterator_impl_base(const iterator_impl_base &) = default;
+        iterator_impl_base(iterator_impl_base &&) noexcept = default;
+        iterator_impl_base &operator=(const iterator_impl_base &) = default;
+        iterator_impl_base &operator=(iterator_impl_base &&) noexcept = default;
+        Derived &operator++()
+        {
+            ++keys_it_;
+            ++values_it_;
+            return static_cast<Derived &>(*this);
+        }
+
+        Derived operator++(int)
+        {
+            auto copy = static_cast<Derived &>(*this);
+            ++(*this);
+            return copy;
+        }
+
+        Derived &operator--()
+        {
+            --keys_it_;
+            --values_it_;
+            return static_cast<Derived &>(*this);
+        }
+
+        Derived operator--(int)
+        {
+            auto copy = static_cast<Derived &>(*this);
+            --(*this);
+            return copy;
+        }
+
+        Derived &operator+=(difference_type n)
+        {
+            keys_it_ += n;
+            values_it_ += n;
+            return static_cast<Derived &>(*this);
+        }
+
+        Derived &operator-=(difference_type n)
+        {
+            keys_it_ -= n;
+            values_it_ -= n;
+            return static_cast<Derived &>(*this);
+        }
+
+        Derived operator+(difference_type n) const
+        {
+            return Derived(keys_it_ + n, values_it_ + n);
+        }
+
+        Derived operator-(difference_type n) const
+        {
+            return Derived(keys_it_ - n, values_it_ - n);
+        }
+
+        bool operator==(const iterator_impl_base &other) const
+        {
+            return keys_it_ == other._keys_it;
+        }
+
+        std::strong_ordering operator<=>(iterator_impl_base &other) const
+        {
+            return keys_it_ <=> other._keys_it;
+        }
+
+    protected:
+        KeysIterator keys_it_;
+        ValuesIterator values_it_;
+    };
 
 public:
     struct containers {
@@ -62,5 +141,4 @@ private:
     containers containers_;  // +0
     key_compare compare_;    // +32
 };
-
 };  // namespace brstd
