811ff4cfa3df82d074cd77ba5fb108914fc956e6
Vincent <magicdroidx@gmail.com>
2025-12-10 21:21:44 +0000

    refactor: simplify `DeathInfoPacket` struct

--- src/bedrock/network/packet/death_info_packet.h
+++ src/bedrock/network/packet/death_info_packet.h
@@ -20,23 +20,18 @@
 #include "bedrock/network/packet/cerealize/core/packet_serialization_helper.h"
 #include "bedrock/network/packet/serialize/serialized_packet.h"
 
-struct DeathInfoPacketInfo {
-    static constexpr auto PACKET_NAME = "DeathInfoPacket";
-    static constexpr auto PACKET_ID = MinecraftPacketIds::DeathInfo;
-    static constexpr SerializationMode DEFAULT_PACKET_SERIALIZATION_MODE = SerializationMode::SideBySide_LogOnMismatch;
-    static constexpr Compressibility COMPRESSIBILITY = Compressibility::Compressible;
-};
-
 struct DeathInfoPacketPayload {
     using DeathCauseMessageType =
         TypeWrapper<std::pair<std::string, std::vector<std::string>>, struct TagDeathCauseMessageType>;
     DeathInfoPacketPayload();
     DeathInfoPacketPayload(const DeathCauseMessageType &);
-
     DeathCauseMessageType death_cause_message;
 };
 
-class DeathInfoPacket : public SerializedPayloadPacket<DeathInfoPacketInfo, DeathInfoPacketPayload> {
+class DeathInfoPacket : public Packet {
 public:
-    static constexpr bool SHARE_WITH_HANDLER = false;
+    DeathInfoPacketPayload payload;
+    SerializationMode serialization_mode = SerializationMode::CerealOnly;
+    DeathInfoPacket();
+    DeathInfoPacket(DeathInfoPacketPayload payload);
 };
