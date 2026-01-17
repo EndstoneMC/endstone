20bb104e03d24e8169b7bae24022a32be827f17d
Vincent <magicdroidx@gmail.com>
2026-01-17 19:08:44 +0000

    refactor: implement `LiquidBlock::_isLiquidBlocking`

--- src/bedrock/world/level/block/liquid_block.cpp
+++ src/bedrock/world/level/block/liquid_block.cpp
@@ -1,5 +1,6 @@
 #include "bedrock/world/level/block/liquid_block.h"
 
+#include "bedrock/world/level/block/components/block_liquid_detection_component.h"
 #include "bedrock/world/level/block_source.h"
 
 bool LiquidBlock::_canSpreadTo(BlockSource &region, BlockPos const &pos, BlockPos const &flow_from_pos,
@@ -16,4 +17,24 @@ bool LiquidBlock::_canSpreadTo(BlockSource &region, BlockPos const &pos, BlockPo
         return false;
     }
     return _isLiquidBlocking(region, pos, flow_from_pos, flow_from_direction);
+}
+
+bool LiquidBlock::_isLiquidBlocking(BlockSource &region, BlockPos const &pos, BlockPos const &flow_from_pos,
+                                    FacingID flow_from_direction) const
+{
+    if (auto &block = region.getLiquidBlock(flow_from_pos); block.getMaterial().isType(MaterialType::Lava)) {
+        return block.isLavaBlocking();
+    }
+    auto get_block = [&region](const BlockPos &block_pos) -> const Block & {
+        return region.getBlock(block_pos);
+    };
+    if (BlockLiquidDetectionComponent::isLiquidBlocking(region.getBlock(pos))) {
+        return true;
+    }
+    if (!BlockLiquidDetectionComponent::liquidCanFlowIntoFromDirection(region.getBlock(pos), flow_from_direction,
+                                                                       get_block, flow_from_pos)) {
+        return true;
+    }
+    return !BlockLiquidDetectionComponent::liquidCanFlowIntoFromDirection(
+        region.getBlock(flow_from_pos), Facing::OPPOSITE_FACING[flow_from_direction], get_block, flow_from_pos);
 }
