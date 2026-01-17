d4c936d3510d60c4cfa2c16b847c34b003f834ee
Vincent <magicdroidx@gmail.com>
2026-01-17 18:17:11 +0000

    refactor: rename BlockFromEvent to BlockFormEvent

--- include/endstone/event/block/block_form_event.h
+++ include/endstone/event/block/block_form_event.h
@@ -17,10 +17,22 @@
 #include "endstone/event/block/block_grow_event.h"
 
 namespace endstone {
-
-class BlockFromEvent final : public BlockGrowEvent {
+/**
+ * @brief Called when a block is formed or spreads based on world conditions.
+ *
+ * Use BlockSpreadEvent to catch blocks that actually spread and don't just "randomly" form.
+ *
+ * Examples:
+ * - Snow forming due to a snow storm.
+ * - Ice forming in a snowy Biome like Taiga or Tundra.
+ * - Obsidian / Cobblestone forming due to contact with water.
+ * - Concrete forming due to mixing of concrete powder and water.
+ *
+ * If a Block Form event is cancelled, the block will not be formed.
+ */
+class BlockFormEvent final : public BlockGrowEvent {
 public:
-    ENDSTONE_EVENT(BlockFormEvent)
+    ENDSTONE_EVENT(BlockFormEvent);
     using BlockGrowEvent::BlockGrowEvent;
 };
 
