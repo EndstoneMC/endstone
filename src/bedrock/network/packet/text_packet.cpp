4cb32e8d7c7ba05679002f3dae132e5fa2a57fc8
Vincent <magicdroidx@gmail.com>
2026-02-10 18:14:28 +0000

    feat(network): update `TextPacket::write`
    Finally, mojang

--- src/bedrock/network/packet/text_packet.cpp
+++ src/bedrock/network/packet/text_packet.cpp
@@ -53,34 +53,21 @@ std::string_view TextPacket::getName() const
 
 void TextPacket::write(BinaryStream &stream) const
 {
-    // TODO(wtf): this doesn't look right, why would we transmit a list of TextPacketType(s) in string to the client?
     stream.writeBool(payload.localize, "Localize?", nullptr);
     stream.writeUnsignedVarInt(payload.body.index(), "Message content (variant based on message type)", nullptr);
     std::visit(
         [&](auto &&arg) {  //
             using T = std::decay_t<decltype(arg)>;
             if constexpr (std::is_same_v<T, TextPacketPayload::MessageOnly>) {
-                stream.writeString("raw", "raw", nullptr);
-                stream.writeString("tip", "tip", nullptr);
-                stream.writeString("systemMessage", "systemMessage", nullptr);
-                stream.writeString("textObjectWhisper", "textObjectWhisper", nullptr);
-                stream.writeString("textObjectAnnouncement", "textObjectAnnouncement", nullptr);
-                stream.writeString("textObject", "textObject", nullptr);
                 stream.writeByte(static_cast<std::uint8_t>(arg.type), "Message Type", nullptr);
                 stream.writeString(arg.message, "Message", nullptr);
             }
             else if constexpr (std::is_same_v<T, TextPacketPayload::AuthorAndMessage>) {
-                stream.writeString("chat", "chat", nullptr);
-                stream.writeString("whisper", "whisper", nullptr);
-                stream.writeString("announcement", "announcement", nullptr);
                 stream.writeByte(static_cast<std::uint8_t>(arg.type), "Message Type", nullptr);
                 stream.writeString(arg.author, "Player Name", nullptr);
                 stream.writeString(arg.message, "Message", nullptr);
             }
             else if constexpr (std::is_same_v<T, TextPacketPayload::MessageAndParams>) {
-                stream.writeString("translate", "translate", nullptr);
-                stream.writeString("popup", "popup", nullptr);
-                stream.writeString("jukeboxPopup", "jukeboxPopup", nullptr);
                 stream.writeByte(static_cast<std::uint8_t>(arg.type), "Message Type", nullptr);
                 stream.writeString(arg.message, "Message", nullptr);
                 stream.writeVectorList(arg.params, "Parameter", nullptr, "Parameter List", nullptr);
