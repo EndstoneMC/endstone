55e9bc3f672610ee69f65c713ca8ef9c26efd0f2
killcerr <kill_cerr@outlook.com>
2026-02-14 20:53:52 +0800

    feat(attribute): add some tests

--- tests/endstone_test/src/endstone_test/tests/player/test_attribute.py
+++ tests/endstone_test/src/endstone_test/tests/player/test_attribute.py
@@ -1,7 +1,12 @@
 from endstone import Player
 from endstone import attribute
-def test_get_attributes(player: Player)->None:
+
+
+def test_get_attributes(player: Player) -> None:
     for attr in player.attributes:
-        player.send_message(f"{attr.type}:{attr.base_value},{attr.value},{attr.modifiers}")
-        attr.add_modifier(attribute.AttributeModifier("test:test",100,attribute.AttributeModifier.ADD))
-        player.send_message(f"{attr.type}:{attr.base_value},{attr.value},{attr.modifiers}")
+        player.send_message(
+            f"{attr.type}:{attr.base_value},{attr.value},{attr.modifiers}")
+        attr.add_modifier(attribute.AttributeModifier(
+            "test:test", 100, attribute.AttributeModifier.ADD))
+        player.send_message(
+            f"{attr.type}:{attr.base_value},{attr.value},{attr.modifiers}")
