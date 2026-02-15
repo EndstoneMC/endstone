47aab0acc73a64426c4eefc84579cbeae8a84fe1
killcerr <kill_cerr@outlook.com>
2026-02-15 23:52:28 +0800

    feat(attribute): update test

--- tests/endstone_test/src/endstone_test/tests/player/test_attribute.py
+++ tests/endstone_test/src/endstone_test/tests/player/test_attribute.py
@@ -3,10 +3,8 @@ from endstone import attribute
 
 
 def test_get_attributes(player: Player) -> None:
-    for attr in player.attributes:
-        player.send_message(
-            f"{attr.type}:{attr.base_value},{attr.value},{attr.modifiers}")
-        attr.add_modifier(attribute.AttributeModifier(
-            "test:test", 100, attribute.AttributeModifier.ADD))
-        player.send_message(
-            f"{attr.type}:{attr.base_value},{attr.value},{attr.modifiers}")
+    attr=player.get_attribute(attribute.Attribute.HEALTH)
+    attr.add_modifier(attribute.AttributeModifier("test:test", 100, attribute.AttributeModifier.ADD))
+    assert len(attr.modifiers)==1
+    attr.remove_modifier(attribute.AttributeModifier("test:test", 100, attribute.AttributeModifier.ADD))
+    assert len(attr.modifiers)==0
