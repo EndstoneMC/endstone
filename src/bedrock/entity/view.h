2652e63680457741ae43c2f9b036e701c276528d
Vincent <magicdroidx@gmail.com>
2026-02-19 15:06:18 +0000

    feat: implement type handling in entity view templates

--- src/bedrock/entity/view.h
+++ src/bedrock/entity/view.h
@@ -19,13 +19,80 @@
 template <typename Component>
 class Optional {
 public:
-    using StorageType = entt::constness_as_t<entt::basic_storage<Component, EntityId>, Component>;
-    using ValueType = Component;
+    using storage_type = entt::storage_for_t<Component, EntityId>;
+    using value_type = Component;
+
+    explicit operator bool() const { return entt_storage_->contains(entity_); }
+    value_type &operator*() const { return entt_storage_->get(entity_); }
 
 private:
-    StorageType *entt_storage_;
+    storage_type *entt_storage_;
     EntityId entity_;
 };
 
+namespace details {
+template <typename...>
+struct EntityNatvisHelper {
+    EntityId entity;
+};
+
+template <typename T>
+struct EntityViewer {};
+
+template <typename... Components>
+struct filter_to_type_lists;
+
+template <typename T>
+struct filter_to_type_lists<T> {
+    using returned_type_list = entt::type_list<T>;
+};
+
+template <typename T>
+struct filter_to_type_lists<Optional<T>> {
+    using returned_type_list = entt::type_list<>;
+};
+
+template <typename T, typename... Rest>
+struct filter_to_type_lists<Optional<T>, Rest...> {
+    using returned_type_list = filter_to_type_lists<Rest...>::returned_type_list;
+};
+
+template <typename T, typename... Rest>
+struct filter_to_type_lists<T, Rest...> {
+    using returned_type_list =
+        entt::type_list_cat_t<entt::type_list<T>, typename filter_to_type_lists<Rest...>::returned_type_list>;
+};
+
+// Endstone begins
+template <class TypeList>
+struct view_type_from_list;
+
+template <class... Ts>
+struct view_type_from_list<entt::type_list<Ts...>> {
+    using type = decltype(std::declval<EnTTRegistry &>().template view<Ts...>());
+};
+// Endstone ends
+}  // namespace details
+
 template <typename EntityContextT, typename... Components>
-class ViewT {};
+class ViewT {
+    using returned_type_list = details::filter_to_type_lists<Components...>::returned_type_list;
+    using ViewType = details::view_type_from_list<returned_type_list>::type;
+
+public:
+    auto tryGetAll(const EntityContextT &context) const
+        -> std::optional<decltype(std::declval<ViewType &>().get(context._getEntityId()))>
+    {
+        auto &view = _get();
+        if (view.contains(context._getEntityId())) {
+            return view.get(context._getEntityId());
+        }
+        return std::nullopt;
+    }
+
+private:
+    ViewType &_get() const { return view_; }
+    EntityRegistry &registry_;
+    mutable ViewType view_;
+    details::EntityViewer<ViewType> entity_viewer_;
+};
