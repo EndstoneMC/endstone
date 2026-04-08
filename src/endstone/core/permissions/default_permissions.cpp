21ff17072c11bc7101cc89e9778a8c7b04bf0940
Vincent <magicdroidx@gmail.com>
2026-04-08 22:24:10 +0100

    fix: restrict /restart command to console only

--- src/endstone/core/permissions/default_permissions.cpp
+++ src/endstone/core/permissions/default_permissions.cpp
@@ -106,7 +106,7 @@ Permission &CommandPermissions::registerPermissions(Permission &parent)
                        "Allows the user to reload the configuration and plugins of the server",
                        PermissionDefault::Operator, commands);
     registerPermission(PREFIX + "restart", "restart", "Allows the user to restart the server",
-                       PermissionDefault::Operator, commands);
+                       PermissionDefault::Console, commands);
     registerPermission(PREFIX + "seed", "seed", "Allows the user to view the seed of the level.",
                        PermissionDefault::Operator, commands);
     registerPermission(PREFIX + "status", "status", "Allows the user to view the status of the server",
