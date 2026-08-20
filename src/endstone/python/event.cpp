9b1ee5640d4856bfadd9159cce7193d45d0b8992
Packeting <127834955+Packeting1@users.noreply.github.com>
2026-08-20 20:51:14 +0800

    fix(event): remove duplicate PlayerPickupItemEvent binding

--- src/endstone/python/event.cpp
+++ src/endstone/python/event.cpp
@@ -372,10 +372,6 @@ void init_event(py::module_ &m, py::class_<Event, PyEvent> &event)
         m, "PlayerPickupItemEvent", "Called when a player picks an item up from the ground.")
         .def_property_readonly("item", &PlayerPickupItemEvent::getItem,
                                "The Item picked up by the entity.");
-    py::class_<PlayerPickupItemEvent, PlayerEvent, ICancellable>(
-        m, "PlayerPickupItemEvent", "Called when a player picks an item up from the ground.")
-        .def_property_readonly("item", &PlayerPickupItemEvent::getItem,
-                               "The Item picked up by the entity.");
     py::class_<PlayerQuitEvent, PlayerEvent>(m, "PlayerQuitEvent", "Called when a player leaves a server.")
         .def_property("quit_message", &PlayerQuitEvent::getQuitMessage, &PlayerQuitEvent::setQuitMessage,
                       "The quit message to send to all online players.");
