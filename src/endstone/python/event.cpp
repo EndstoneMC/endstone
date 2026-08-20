bce3f5f2972b2d9616ed36bfbfadbb7e5a20c911
Packeting <127834955+Packeting1@users.noreply.github.com>
2026-08-20 15:22:47 +0800

    refactor(event): reorder player event declarations

--- src/endstone/python/event.cpp
+++ src/endstone/python/event.cpp
@@ -355,14 +355,6 @@ void init_event(py::module_ &m, py::class_<Event, PyEvent> &event)
         .def_property_readonly("new_slot", &PlayerItemHeldEvent::getNewSlot, "The new held slot index.")
         .def_property_readonly("previous_slot", &PlayerItemHeldEvent::getPreviousSlot,
                                "The previous held slot index.");
-    py::class_<PlayerToggleSneakEvent, PlayerEvent>(m, "PlayerToggleSneakEvent",
-                                                    "Called when a player toggles their sneaking state.")
-        .def_property_readonly("is_sneaking", &PlayerToggleSneakEvent::isSneaking,
-                               "Whether the player is now sneaking or not.");
-    py::class_<PlayerToggleSprintEvent, PlayerEvent>(m, "PlayerToggleSprintEvent",
-                                                     "Called when a player toggles their sprinting state.")
-        .def_property_readonly("is_sprinting", &PlayerToggleSprintEvent::isSprinting,
-                               "Whether the player is now sprinting or not.");
     py::class_<PlayerJoinEvent, PlayerEvent>(m, "PlayerJoinEvent", "Called when a player joins a server.")
         .def_property("join_message", &PlayerJoinEvent::getJoinMessage, &PlayerJoinEvent::setJoinMessage,
                       "The join message to send to all online players.");
@@ -380,6 +372,10 @@ void init_event(py::module_ &m, py::class_<Event, PyEvent> &event)
         .def_property("to_location", &PlayerMoveEvent::getTo, &PlayerMoveEvent::setTo,
                       "The location that this player moved to.");
     py::class_<PlayerJumpEvent, PlayerMoveEvent>(m, "PlayerJumpEvent", "Called when a player jumps.");
+    py::class_<PlayerPickupItemEvent, PlayerEvent, ICancellable>(
+        m, "PlayerPickupItemEvent", "Called when a player picks an item up from the ground.")
+        .def_property_readonly("item", &PlayerPickupItemEvent::getItem,
+                               "The Item picked up by the entity.");
     py::class_<PlayerQuitEvent, PlayerEvent>(m, "PlayerQuitEvent", "Called when a player leaves a server.")
         .def_property("quit_message", &PlayerQuitEvent::getQuitMessage, &PlayerQuitEvent::setQuitMessage,
                       "The quit message to send to all online players.");
@@ -403,10 +399,14 @@ void init_event(py::module_ &m, py::class_<Event, PyEvent> &event)
         m, "PlayerTeleportEvent", "Called when a player is teleported from one location to another.");
     py::class_<PlayerPortalEvent, PlayerTeleportEvent>(
         m, "PlayerPortalEvent", "Called when a player is about to teleport because it is in contact with a portal.");
-    py::class_<PlayerPickupItemEvent, PlayerEvent, ICancellable>(
-        m, "PlayerPickupItemEvent", "Called when a player picks an item up from the ground.")
-        .def_property_readonly("item", &PlayerPickupItemEvent::getItem,
-                               "The Item picked up by the entity.");
+    py::class_<PlayerToggleSneakEvent, PlayerEvent>(m, "PlayerToggleSneakEvent",
+                                                    "Called when a player toggles their sneaking state.")
+        .def_property_readonly("is_sneaking", &PlayerToggleSneakEvent::isSneaking,
+                               "Whether the player is now sneaking or not.");
+    py::class_<PlayerToggleSprintEvent, PlayerEvent>(m, "PlayerToggleSprintEvent",
+                                                     "Called when a player toggles their sprinting state.")
+        .def_property_readonly("is_sprinting", &PlayerToggleSprintEvent::isSprinting,
+                               "Whether the player is now sprinting or not.");
 
     // Server events
     py::class_<ServerEvent, Event>(m, "ServerEvent", "Represents a Server-related event.");
