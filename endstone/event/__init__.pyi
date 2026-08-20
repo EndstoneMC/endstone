2dc8c70ad57f4580e7d10164a41919316f99289a
Packeting <127834955+Packeting1@users.noreply.github.com>
2026-08-20 15:17:15 +0800

    refactor(event): reorder player event declarations

--- endstone/event/__init__.pyi
+++ endstone/event/__init__.pyi
@@ -766,26 +766,6 @@ class PlayerItemHeldEvent(PlayerEvent, Cancellable):
         The previous held slot index.
         """
 
-class PlayerToggleSneakEvent(PlayerEvent):
-    """
-    Called when a player toggles their sneaking state.
-    """
-    @property
-    def is_sneaking(self) -> bool:
-        """
-        Whether the player is now sneaking or not.
-        """
-
-class PlayerToggleSprintEvent(PlayerEvent):
-    """
-    Called when a player toggles their sprinting state.
-    """
-    @property
-    def is_sprinting(self) -> bool:
-        """
-        Whether the player is now sprinting or not.
-        """
-
 class PlayerJoinEvent(PlayerEvent):
     """
     Called when a player joins a server.
@@ -850,6 +830,16 @@ class PlayerJumpEvent(PlayerMoveEvent):
     """
     Called when a player jumps.
     """
+
+class PlayerPickupItemEvent(PlayerEvent, Cancellable):
+    """
+    Called when a player picks an item up from the ground.
+    """
+    @property
+    def item(self) -> Item:
+        """
+        The Item picked up by the entity.
+        """
 
 class PlayerQuitEvent(PlayerEvent):
     """
@@ -923,14 +913,24 @@ class PlayerPortalEvent(PlayerTeleportEvent):
     Called when a player is about to teleport because it is in contact with a portal.
     """
 
-class PlayerPickupItemEvent(PlayerEvent, Cancellable):
+class PlayerToggleSneakEvent(PlayerEvent):
     """
-    Called when a player picks an item up from the ground.
+    Called when a player toggles their sneaking state.
     """
     @property
-    def item(self) -> Item:
+    def is_sneaking(self) -> bool:
         """
-        The Item picked up by the entity.
+        Whether the player is now sneaking or not.
+        """
+
+class PlayerToggleSprintEvent(PlayerEvent):
+    """
+    Called when a player toggles their sprinting state.
+    """
+    @property
+    def is_sprinting(self) -> bool:
+        """
+        Whether the player is now sprinting or not.
         """
 
 class ServerEvent(Event):
