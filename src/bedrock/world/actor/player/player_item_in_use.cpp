51417fdd48b9ffb2f8a2922029abb915f96ad980
Vincent <magicdroidx@gmail.com>
2026-08-21 01:54:52 +0000

    revert(bedrock): restore VanillaItemTags
    The branch deleted world/item/vanilla_item_tags.{h,cpp}, dropped the CMake entry
    and inlined the tag id as a string literal in PlayerItemInUse::setItemInUse.
    Nothing about bucket events needs that, and it undoes a reconstruction that
    landed on develop.

--- src/bedrock/world/actor/player/player_item_in_use.cpp
+++ src/bedrock/world/actor/player/player_item_in_use.cpp
@@ -16,6 +16,7 @@
 
 #include "bedrock/entity/components/item_in_use_component.h"
 #include "bedrock/world/item/item.h"
+#include "bedrock/world/item/vanilla_item_tags.h"
 
 const ItemStack &PlayerItemInUse::getItemInUse() const
 {
@@ -40,7 +41,7 @@ void PlayerItemInUse::setItemInUse(const ItemStack &new_item, EntityContext &own
     auto &item_in_use = owner.getOrAddComponent<ItemInUseComponent>();
     item_in_use.item = item_.getItemPtr();
     item_in_use.duration = duration;
-    if (item_.hasTag("minecraft:is_trident")) {
+    if (item_.hasTag(VanillaItemTags::Trident)) {
         owner.getOrAddComponent<ItemInUseTicksDuringMovementComponent>();
     }
 }
